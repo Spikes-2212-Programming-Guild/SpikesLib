60838ae52c3a723c9ad2ffdb2c7600a10a3f82f5
yonatan <yonatan@yonatan-Lenovo-U41-70>
2016-09-02 13:16:11 +0300

    Added DoubleLimitedSubsystem

---
+++ src/com/spikes2212/genericsubsystems/DoubleLimitedSubsystem.java
@@ -0,0 +1,36 @@
+package com.spikes2212.genericsubsystems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public abstract class DoubleLimitedSubsystem extends Subsystem {
+	private SpeedController motor;
+
+	public DoubleLimitedSubsystem(SpeedController motor) {
+		this.motor = motor;
+	}
+
+	public abstract boolean isMin();
+
+	public abstract boolean isMax();
+
+	public boolean canMove(double speed) {
+		return !(speed < 0 && isMin() || speed > 0 && isMax());
+	}
+
+	public void tryMove(double speed) {
+		if (canMove(speed)) {
+			motor.set(speed);
+		}
+	}
+
+	public void stop() {
+		motor.set(0);
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+}
