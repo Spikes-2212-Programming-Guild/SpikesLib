84e9f092f1bf83a7218509261daaec861d9fd55f
shakedb70 <shakedb70@gmail.com>
2016-09-08 20:02:13 +0300

    changed to abstract class

--- src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrive.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrive.java
@@ -1,6 +1,6 @@
 package com.spikes2212.genericsubsystems.drivetrains;
 
-public interface HolonomicDrive extends TankDrive {
-	public void holonomicDrive(double speedX, double speedY); // x-forward/backward
+public abstract class HolonomicDrive extends TankDrive {
+	public abstract void holonomicDrive(double speedX, double speedY); // x-forward/backward
 																// y-left/right
 }
--- src/com/spikes2212/genericsubsystems/drivetrains/TankDrive.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/TankDrive.java
@@ -1,5 +1,7 @@
 package com.spikes2212.genericsubsystems.drivetrains;
 
-public interface TankDrive {
-	public void tankDrive(double spLeft, double spRight);
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public abstract class TankDrive extends Subsystem {
+	public abstract void tankDrive(double spLeft, double spRight);
 }
