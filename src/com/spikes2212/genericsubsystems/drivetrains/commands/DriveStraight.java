ced6b72e01eff7aca57defc380c8a3fbafa5db47
shakedb70 <shakedb70@gmail.com>
2016-09-08 20:15:02 +0300

    added DriveStrait

---
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveStraight.java
@@ -0,0 +1,46 @@
+package com.spikes2212.genericsubsystems.drivetrains.commands;
+
+import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveStraight extends Command {
+
+	private TankDrivetrain tankDrivetrain;
+	private double speed;
+
+	public DriveStraight(TankDrivetrain drivetrain, double speed) {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(drivetrain);
+		this.tankDrivetrain = drivetrain;
+		this.speed = speed;
+
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		tankDrivetrain.tankDrive(speed, speed);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return isTimedOut();
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
