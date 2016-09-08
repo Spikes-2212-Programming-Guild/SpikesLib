d7c5ea3d5d3846e2879f05c52d0c56d0d5f5ebe1
shakedb70 <shakedb70@gmail.com>
2016-09-08 20:14:23 +0300

    rename

--- src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrivetrain.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrivetrain.java
@@ -1,6 +1,6 @@
 package com.spikes2212.genericsubsystems.drivetrains;
 
-public abstract class HolonomicDrive extends TankDrive {
+public abstract class HolonomicDrivetrain extends TankDrivetrain {
 	public abstract void holonomicDrive(double speedX, double speedY); // x-forward/backward
 																// y-left/right
 }
--- src/com/spikes2212/genericsubsystems/drivetrains/TankDrivetrain.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/TankDrivetrain.java
@@ -2,6 +2,6 @@ package com.spikes2212.genericsubsystems.drivetrains;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public abstract class TankDrive extends Subsystem {
+public abstract class TankDrivetrain extends Subsystem {
 	public abstract void tankDrive(double spLeft, double spRight);
 }
