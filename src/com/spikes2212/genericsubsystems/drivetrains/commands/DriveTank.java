aa5b04ac797dc8ea6359d51e366f39f3cd855790
shakedb70 <shakedb70@gmail.com>
2016-09-14 19:56:58 +0300

    adding end to DriveTank

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveTank.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveTank.java
@@ -37,10 +37,12 @@ public class DriveTank extends Command {
 
 	// Called once after isFinished returns true
 	protected void end() {
+		tankDrivetrain.tankDrive(0, 0);
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
+		end();
 	}
 }
