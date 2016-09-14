c13302da5ce3dc2d78260b62061b0f93492059de
shakedb70 <shakedb70@gmail.com>
2016-09-14 18:28:50 +0300

    switched between x to y

--- src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrivetrain.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrivetrain.java
@@ -3,7 +3,7 @@ package com.spikes2212.genericsubsystems.drivetrains;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public abstract class HolonomicDrivetrain extends TankDrivetrain {
-	public abstract void holonomicDrive(double speedX, double speedY); // x-forward/backward
-																// y-left/right
+	public abstract void holonomicDrive(double speedY, double speedX); // y-forward/backward
+																// x-left/right
 	
 }
