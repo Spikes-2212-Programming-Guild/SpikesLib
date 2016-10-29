9d3003208d9a67039bb19fba22344b51899b5161
yonatan <yonatan@yonatan-Lenovo-U41-70>
2016-10-29 11:15:59 +0300

    fixed things with monitor

--- src/com/spikes2212/utils/Monitor.java
+++ src/com/spikes2212/utils/Monitor.java
@@ -6,29 +6,31 @@ import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 public class Monitor {
 	private static PowerDistributionPanel pdp;
-	private static double high_voltage = 10;
-	private static double low_voltage = 7;
+	private static double highVoltage = 10;
+	private static double lowVoltage = 7;
 
 	public Monitor() {
 		pdp = new PowerDistributionPanel();
 	}
 
-	public static void setHigh_voltage(double high_voltage) {
-		Monitor.high_voltage = high_voltage;
+	public static void setHigh_voltage(double highVoltage) {
+		if (highVoltage > lowVoltage)
+			Monitor.highVoltage = highVoltage;
 	}
 
-	public static void setLow_voltage(double low_voltage) {
-		Monitor.low_voltage = low_voltage;
+	public static void setLow_voltage(double lowVoltage) {
+		if (highVoltage > lowVoltage)
+			Monitor.lowVoltage = lowVoltage;
 	}
 
 	public Supplier<Double> monitorSupplier(Supplier<Double> supplier) {
 		double voltage = pdp.getVoltage();
-		if (voltage < low_voltage)
+		if (voltage < lowVoltage)
 			return () -> supplier.get() * 0;
-		else if (voltage > high_voltage)
+		else if (voltage > highVoltage)
 			return () -> supplier.get() * 1;
 		else
-			return () -> supplier.get() * (voltage - low_voltage) / (high_voltage - low_voltage);
+			return () -> supplier.get() * (voltage - lowVoltage) / (highVoltage - lowVoltage);
 	}
 
 }
