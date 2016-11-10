1933a8bee59748f414b916a734076b6f23ddddd2
Spikes Programming Guild <spikes.no2212@gmail.com>
2016-11-10 19:39:45 +0200

    Priorities added

--- src/com/spikes2212/utils/VoltageMonitor.java
+++ src/com/spikes2212/utils/VoltageMonitor.java
@@ -6,20 +6,29 @@ import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 public class VoltageMonitor {
 	private static PowerDistributionPanel pdp = new PowerDistributionPanel();
-	private static double highVoltage = 10;
-	private static double lowVoltage = 7;
+	private static double defaultHighVoltage = 10;
+	private static double defaultLowVoltage = 7;
+
+	public enum Priority {
+		LOW (1), HIGH (-1), DEFAULT (0);
+		private double volageDifference;
+		private Priority (double voltageDifference){
+			this.volageDifference = voltageDifference;
+		}
+	}
 
 	public static void setHigh_voltage(double highVoltage) {
-		if (highVoltage > lowVoltage)
-			VoltageMonitor.highVoltage = highVoltage;
+		if (highVoltage > defaultLowVoltage)
+			VoltageMonitor.defaultHighVoltage = highVoltage;
 	}
 
 	public static void setLow_voltage(double lowVoltage) {
-		if (highVoltage > lowVoltage)
-			VoltageMonitor.lowVoltage = lowVoltage;
+		if (defaultHighVoltage > lowVoltage)
+			VoltageMonitor.defaultLowVoltage = lowVoltage;
 	}
 
-	public static Supplier<Double> monitorSupplier(Supplier<Double> supplier) {
+	public static Supplier<Double> monitorSupplier(Supplier<Double> supplier,
+			double highVoltage, double lowVoltage) {
 		return () -> {
 			double voltage = pdp.getVoltage();
 			if (voltage < lowVoltage)
@@ -27,8 +36,18 @@ public class VoltageMonitor {
 			else if (voltage > highVoltage)
 				return supplier.get() * 1;
 			else
-				return supplier.get() * (voltage - lowVoltage) / (highVoltage - lowVoltage);
+				return supplier.get() * (voltage - lowVoltage)
+						/ (highVoltage - lowVoltage);
 		};
 	}
+
+	public static Supplier<Double> monitorSupplier(Supplier<Double> supplier) {
+		return monitorSupplier(supplier, defaultHighVoltage, defaultLowVoltage);
+	}
+	
+	public static Supplier<Double> monitorSupplier(Supplier<Double> supplier, Priority priority) {
+		return monitorSupplier(supplier, defaultHighVoltage + priority.volageDifference, defaultLowVoltage + priority.volageDifference);
+	}
+
 
 }
