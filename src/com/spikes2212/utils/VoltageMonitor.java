045f239a8c67c11f9025ca5361065e3531a1a982
Spikes Programming Guild <spikes.no2212@gmail.com>
2016-11-10 18:50:18 +0200

    fix voltage monitor

--- src/com/spikes2212/utils/VoltageMonitor.java
+++ src/com/spikes2212/utils/VoltageMonitor.java
@@ -5,13 +5,9 @@ import java.util.function.Supplier;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 public class VoltageMonitor {
-	private static PowerDistributionPanel pdp;
+	private static PowerDistributionPanel pdp = new PowerDistributionPanel();
 	private static double highVoltage = 10;
 	private static double lowVoltage = 7;
-
-	public VoltageMonitor() {
-		pdp = new PowerDistributionPanel();
-	}
 
 	public static void setHigh_voltage(double highVoltage) {
 		if (highVoltage > lowVoltage)
@@ -24,13 +20,15 @@ public class VoltageMonitor {
 	}
 
 	public static Supplier<Double> monitorSupplier(Supplier<Double> supplier) {
-		double voltage = pdp.getVoltage();
-		if (voltage < lowVoltage)
-			return () -> supplier.get() * 0;
-		else if (voltage > highVoltage)
-			return () -> supplier.get() * 1;
-		else
-			return () -> supplier.get() * (voltage - lowVoltage) / (highVoltage - lowVoltage);
+		return () -> {
+			double voltage = pdp.getVoltage();
+			if (voltage < lowVoltage)
+				return supplier.get() * 0;
+			else if (voltage > highVoltage)
+				return supplier.get() * 1;
+			else
+				return supplier.get() * (voltage - lowVoltage) / (highVoltage - lowVoltage);
+		};
 	}
 
 }
