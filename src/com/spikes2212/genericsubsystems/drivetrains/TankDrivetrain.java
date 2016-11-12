351172ad16b65b50bc26bcb9420bc43fadfd96fd
Leon <Leon@LeonLaptop>
2016-11-12 15:07:34 +0200

    made tankDriveTrain more suitble for pid

--- src/com/spikes2212/genericsubsystems/drivetrains/TankDrivetrain.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/TankDrivetrain.java
@@ -1,8 +1,23 @@
 package com.spikes2212.genericsubsystems.drivetrains;
 
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public abstract class TankDrivetrain extends Subsystem {
-	public abstract void tankDrive(double speedLeft, double speedRight);
+	public void tankDrive(double speedLeft, double speedRight) {
+		setLeft(speedLeft);
+		setRight(speedRight);
+	}
+
+	public abstract void setLeft(double speedLeft);
+
+	public abstract void setRight(double speedRight);
+
+	public final PIDOutput PIDOutputLeft = this::setLeft;
+	public final PIDOutput PIDOutputRight = this::setRight;
+
+	public abstract PIDSource getLeftPIDSource();
+	public abstract PIDSource getRightPIDSource();
 
 }
