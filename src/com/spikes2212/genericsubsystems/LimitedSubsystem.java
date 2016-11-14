5ca8e4f6d7a35e563706b3832ac37f2942d7d4cb
mantin2212 <noam.mantin@gmail.com>
2016-11-14 19:10:25 +0200

    added docs to limited subsystem

--- src/com/spikes2212/genericsubsystems/LimitedSubsystem.java
+++ src/com/spikes2212/genericsubsystems/LimitedSubsystem.java
@@ -10,20 +10,46 @@ public abstract class LimitedSubsystem extends Subsystem {
 		this.motor = motor;
 	}
 
+	/**
+	 * checks whether the subsystem reached it's minimum limit
+	 * 
+	 * @return true if the subsystem is in the low limit and false otherwise
+	 */
 	public abstract boolean isMin();
 
+	/**
+	 * checks whether the subsystem reached it's maximum limit
+	 * 
+	 * @return true if the subsystem is in the high limit and false otherwise
+	 */
 	public abstract boolean isMax();
 
+	/**
+	 * Checks whether the subsystem can move at a certain speed
+	 * 
+	 * @param speed
+	 *            represents the speed which is checked
+	 * @return true if the subsystem can move at the chosen speed
+	 */
 	public boolean canMove(double speed) {
 		return !(speed < 0 && isMin() || speed > 0 && isMax());
 	}
 
+	/**
+	 * Try to move this subsystem at a certain speed
+	 * 
+	 * @param speed
+	 *            represents the speed at which we should try and turn the motor
+	 */
 	public void tryMove(double speed) {
 		if (canMove(speed)) {
 			motor.set(speed);
 		}
 	}
 
+	/**
+	 * stops the subsystem
+	 */
 	public void stop() {
 		motor.set(0);
 	}
