eaae01f88bf383aa69d41e46d1067b6641b6f938
omri cohen <omrichoen@gmail.com>
2016-12-03 10:32:00 +0200

    made MonitoredMoveLimitedSubsystem depend on the unmonitored version

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/MonitoredDriveHolonomic.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/MonitoredDriveHolonomic.java
@@ -11,50 +11,10 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class MonitoredDriveHolonomic extends Command {
-	private HolonomicDrivetrain holonomicDrivetrain;
-	private Supplier<Double> speedYSupplier, speedXSupplier;
-
-	public MonitoredDriveHolonomic(HolonomicDrivetrain drivetrain, double speedY, double speedX) {
-		// Use requires() here to declare subsystem dependencieslier
-		// eg. requires(chassis);
-		this(drivetrain, () -> speedY, () -> speedX);
-
-	}
+public class MonitoredDriveHolonomic extends DriveHolonomic {
 
 	public MonitoredDriveHolonomic(HolonomicDrivetrain drivetrain, Supplier<Double> speedYSupplier,
 			Supplier<Double> speedXSupplier) {
-		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
-		requires(drivetrain);
-		this.holonomicDrivetrain = drivetrain;
-		this.speedXSupplier = VoltageMonitor.monitorSupplier(speedXSupplier);
-		this.speedYSupplier = VoltageMonitor.monitorSupplier(speedYSupplier);
-	}
-
-	// Called just before this Command runs the first time
-	protected void initialize() {
-	}
-
-	// Called repeatedly when this Command is scheduled to run
-	protected void execute() {
-		holonomicDrivetrain.holonomicDrive(speedYSupplier.get(), speedXSupplier.get());
-
-	}
-
-	// Make this return true when this Command no longer needs to run execute()
-	protected boolean isFinished() {
-		return isTimedOut();
-	}
-
-	// Called once after isFinished returns true
-	protected void end() {
-		holonomicDrivetrain.holonomicDrive(0, 0);
-	}
-
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	protected void interrupted() {
-		end();
+		super(drivetrain,VoltageMonitor.monitorSupplier(speedYSupplier),VoltageMonitor.monitorSupplier(speedXSupplier));
 	}
 }
--- src/com/spikes2212/genericsubsystems/drivetrains/commands/MonitoredDriveTank.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/MonitoredDriveTank.java
@@ -10,50 +10,11 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class MonitoredDriveTank extends Command {
-
-	private TankDrivetrain tankDrivetrain;
-	private Supplier<Double> leftSpeedSuplier;
-	private Supplier<Double> rightSpeedSuplier;
-
-	public MonitoredDriveTank(TankDrivetrain drivetrain, double leftSpeed, double rightSpeed) {
-		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
-		this(drivetrain, () -> leftSpeed, () -> rightSpeed);
-	}
+public class MonitoredDriveTank extends DriveTank {
 
 	public MonitoredDriveTank(TankDrivetrain drivetrain, Supplier<Double> leftSpeedSupplier,
 			Supplier<Double> rightSpeedSupplier) {
-		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
-		requires(drivetrain);
-		this.tankDrivetrain = drivetrain;
-		this.leftSpeedSuplier = VoltageMonitor.monitorSupplier(leftSpeedSupplier);
-		this.rightSpeedSuplier = VoltageMonitor.monitorSupplier(rightSpeedSupplier);
+		super (drivetrain,VoltageMonitor.monitorSupplier(leftSpeedSupplier),VoltageMonitor.monitorSupplier(rightSpeedSupplier));
 	}
 
-	// Called just before this Command runs the first time
-	protected void initialize() {
-	}
-
-	// Called repeatedly when this Command is scheduled to run
-	protected void execute() {
-		tankDrivetrain.tankDrive(leftSpeedSuplier.get(), rightSpeedSuplier.get());
-	}
-
-	// Make this return true when this Command no longer needs to run execute()
-	protected boolean isFinished() {
-		return isTimedOut();
-	}
-
-	// Called once after isFinished returns true
-	protected void end() {
-		tankDrivetrain.tankDrive(0, 0);
-	}
-
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	protected void interrupted() {
-		end();
-	}
 }
