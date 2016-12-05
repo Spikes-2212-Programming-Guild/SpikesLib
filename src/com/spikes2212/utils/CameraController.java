f39e397f5358ad0d3d83bffba6262fcc11d9a5ff
mantin2212 <noam.mantin@gmail.com>
2016-12-05 20:16:28 +0200

    added camera handler

---
+++ src/com/spikes2212/utils/CameraController.java
@@ -0,0 +1,63 @@
+package com.spikes2212.utils;
+
+import java.util.Optional;
+
+import com.ni.vision.NIVision.Image;
+
+import edu.wpi.first.wpilibj.vision.USBCamera;
+
+public class CameraController {
+	public static int resolutionWidth = 100, resolutionHeight = 100;
+	private Optional<USBCamera> camera;
+	private boolean on;
+
+	public CameraController(String port, int fps) {
+		try {
+			USBCamera camera = new USBCamera(port);
+			camera.setFPS(fps);
+			camera.setSize(resolutionWidth, resolutionHeight);
+			camera.setExposureAuto();
+			camera.updateSettings();
+			this.camera = Optional.of(camera);
+		} catch (Exception e) {
+			e.printStackTrace();
+			this.camera = Optional.empty();
+		}
+	}
+
+	public void start() {
+		if (camera.isPresent()) {
+			try {
+				camera.get().startCapture();
+				on = true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void stop() {
+		try {
+			camera.ifPresent(USBCamera::stopCapture);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		on = false;
+	}
+
+	public void getImage(Image image) {
+		try {
+			camera.ifPresent(c -> c.getImage(image));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean hasCamera() {
+		return camera.isPresent();
+	}
+
+	public boolean isOn() {
+		return on;
+	}
+}
---
+++ src/com/spikes2212/utils/CamerasHandler.java
@@ -0,0 +1,51 @@
+package com.spikes2212.utils;
+
+import java.util.ArrayList;
+
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ImageType;
+
+import edu.wpi.first.wpilibj.CameraServer;
+
+public class CamerasHandler {
+	private ArrayList<CameraController> cameras;
+	private CameraController currentCamera = null;
+	private Image image;
+	public static int fps = 30;
+
+	public CamerasHandler() {
+		cameras = new ArrayList<CameraController>();
+		this.image = NIVision.imaqCreateImage(ImageType.IMAGE_RGB, 0);
+	}
+
+	public void addCamera(String port) {
+		cameras.add(new CameraController(port, fps));
+	}
+
+	public void stop() {
+		cameras.forEach(c -> c.stop());
+		currentCamera = null;
+	}
+
+	public void start(int index) {
+		if (currentCamera != cameras.get(index)) {
+			stop();
+			cameras.get(index).start();
+			currentCamera = cameras.get(index);
+		}
+	}
+
+	public void getImage(Image image) {
+		currentCamera.getImage(image);
+	}
+
+	public synchronized void tryStream() {
+		getImage(image);
+		CameraServer.getInstance().setImage(image);
+	}
+
+	public boolean isOn() {
+		return currentCamera != null;
+	}
+}
