0e987a51149550d468da96590dc8d74f3f0c30ce
Tomer <Tomer@192.168.1.117>
2016-12-08 18:16:54 +0200

    got XboXUID

---
+++ src/com/spikes2212/utils/XboXUID.java
@@ -0,0 +1,80 @@
+package com.spikes2212.utils;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+public class XboXUID extends Joystick {
+
+	static final int X_RIGHT = 0;
+	static final int X_LEFT = 1;
+	static final int Y_RIGHT = 2;
+	static final int Y_LEFT = 3;
+
+	public XboXUID(int port) {
+		super(port);
+
+	}
+
+	public Button getGreenButton() {
+		return new JoystickButton(this, 2);
+	}
+
+	public Button getBlueButton() {
+		return new JoystickButton(this, 1);
+	}
+
+	public Button getRedButton() {
+		return new JoystickButton(this, 3);
+	}
+
+	public Button getYellowButton() {
+		return new JoystickButton(this, 4);
+	}
+
+	public Button getRtButton() {
+		return new JoystickButton(this, 8);
+	}
+
+	public Button getRbButton() {
+		return new JoystickButton(this, 6);
+	}
+
+	public Button getLtButton() {
+		return new JoystickButton(this, 7);
+	}
+
+	public Button getLbButton() {
+		return new JoystickButton(this, 5);
+	}
+
+	public double getRightX() {
+		return getRawAxis(X_RIGHT);
+	}
+
+	public double getRightY() {
+		return getRawAxis(Y_RIGHT);
+	}
+
+	public double getLefttX() {
+		return getRawAxis(X_LEFT);
+	}
+
+	public double getLeftY() {
+		return getRawAxis(Y_RIGHT);
+	}
+	public Button getUpButton() {
+		return new JoystickButton(this, 14);
+}
+	public Button getDownButton() {
+		return new JoystickButton(this,15 );
+}
+	public Button getLeftButton() {
+		return new JoystickButton(this, 16);
+}
+	public Button getRightButton() {
+		return new JoystickButton(this,17 );
+}
+}
