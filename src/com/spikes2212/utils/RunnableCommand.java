582b52620064dd1bdd152d47111bf3c595f74f12
Tomer <Tomer@192.168.1.123>
2016-12-13 18:42:19 +0200

    added isFinished method

--- src/com/spikes2212/utils/RunnableCommand.java
+++ src/com/spikes2212/utils/RunnableCommand.java
@@ -7,33 +7,33 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class RunnableCommand extends Command {
-	
+
 	private Runnable runnable;
-	
+
 	public RunnableCommand(Runnable runnable) {
 		this.runnable = runnable;
-    }
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	new Thread(runnable).start();
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		new Thread(runnable).start();
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return true;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
