47f36107c7ef2cae8200d4c1c134856b170f5b9e
Tomer <Tomer@192.168.1.123>
2016-12-13 18:59:30 +0200

    finished

--- src/com/spikes2212/Cameras/ChangeCamera.java
+++ src/com/spikes2212/Cameras/ChangeCamera.java
@@ -13,7 +13,7 @@ public class ChangeCamera extends RunnableCommand {
 
 		super(() -> handler.start(index));
 		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
+		// eg. requires(chassis); 
 	}
 
 	// Called just before this Command runs the first time
