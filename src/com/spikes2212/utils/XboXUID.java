c3a22d5c6de853e971477a5329e2c4f1af001717
roeeiit1 <roeeiit1@gmail.com>
2016-12-15 19:04:16 +0200

    Revert "Fixed XboXUID, again."
    This reverts commit 5d13a96fae4129e7c8d769dfc6eac89c795600a0.

--- src/com/spikes2212/utils/XboXUID.java
+++ src/com/spikes2212/utils/XboXUID.java
@@ -8,77 +8,66 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class XboXUID extends Joystick {
 
-    static final int X_RIGHT = 2;
-    static final int X_LEFT = 0;
-    static final int Y_RIGHT = 3;
-    static final int Y_LEFT = 1;
+	static final int X_RIGHT = 2;
+	static final int X_LEFT = 0;
+	static final int Y_RIGHT = 3;
+	static final int Y_LEFT = 1;
 
-    public XboXUID(int port) {
-        super(port);
-    }
+	public XboXUID(int port) {
+		super(port);
+	}
 
-    public Button getGreenButton() {
-        return new JoystickButton(this, 2);
-    }
+	public Button getGreenButton() { return new JoystickButton(this, 2); }
 
-    public Button getBlueButton() {
-        return new JoystickButton(this, 1);
-    }
+	public Button getBlueButton() { return new JoystickButton(this, 1); }
 
-    public Button getRedButton() {
-        return new JoystickButton(this, 3);
-    }
+	public Button getRedButton() {
+		return new JoystickButton(this, 3);
+	}
 
-    public Button getYellowButton() {
-        return new JoystickButton(this, 4);
-    }
+	public Button getYellowButton() {
+		return new JoystickButton(this, 4);
+	}
+1
+	public Button getRtButton() {
+		return new JoystickButton(this, 8);
+	}
 
-    public Button getRtButton() {
-        return new JoystickButton(this, 8);
-    }
+	public Button getRbButton() {
+		return new JoystickButton(this, 6);
+	}
 
-    public Button getRbButton() {
-        return new JoystickButton(this, 6);
-    }
+	public Button getLtButton() {
+		return new JoystickButton(this, 7);
+	}
 
-    public Button getLtButton() {
-        return new JoystickButton(this, 7);
-    }
+	public Button getLbButton() {
+		return new JoystickButton(this, 5);
+	}
 
-    public Button getLbButton() {
+	public double getRightX() {
+		return getRawAxis(X_RIGHT);
+	}
 
-        return new JoystickButton(this, 5);
-    }
+	public double getRightY() {
+		return getRawAxis(Y_RIGHT);
+	}
 
-    public double getRightX() {
-        return getRawAxis(X_RIGHT)
-    }
+	public double getLeftX() {
+		return getRawAxis(X_LEFT);
+	}
 
-    public double getRightY() {
-        return getRawAxis(Y_RIGHT);
-    }
-
-    public double getLeftX() {
-        return getRawAxis(X_LEFT);
-    }
-
-    public double getLeftY() {
-        return getY(); // Do not edit this to "getRawAxis(Y_LEFT)", it somehow works this way
-    }
-
-    public Button getUpButton() {
-        return new JoystickButton(this, 14);
-    }
-
-    public Button getDownButton() {
-        return new JoystickButton(this, 15);
-    }
-
-    public Button getLeftButton() {
-        return new JoystickButton(this, 16);
-    }
-
-    public Button getRightButton() {
-        return new JoystickButton(this, 17);
-    }
+	public double getLeftY() {
+		return getY(); // Do not edit this to "getRawAxis(Y_LEFT)", it somehow works this way
+	}
+	public Button getUpButton() {
+		return new JoystickButton(this, 14);
+}
+	public Button getDownButton() {
+		return new JoystickButton(this,15);
+}
+	public Button getLeftButton() {
+		return new JoystickButton(this, 16);
+}
+	public Button getRightButton() { return new JoystickButton(this, 17); }
 }
