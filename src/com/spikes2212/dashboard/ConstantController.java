37c880742529bd45dcbea47fbadf8013eb4d404e
omri cohen <omrichoen@gmail.com>
2017-01-13 13:41:24 +0200

    added more types of constants

--- src/com/spikes2212/dashboard/ConstantController.java
+++ src/com/spikes2212/dashboard/ConstantController.java
@@ -5,9 +5,18 @@ import java.util.function.Supplier;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ConstantController {
-	public static Supplier<Double> addConstant(String name, double value) {
+	public static Supplier<Double> addConstantDouble(String name, double value) {
 		SmartDashboard.putNumber(name, value);
 		return () -> SmartDashboard.getNumber(name);
+	}
+
+	public static Supplier<Integer> addConstantInt(String name, int value) {
+		SmartDashboard.putNumber(name, value);
+		return () -> (int) SmartDashboard.getNumber(name);
+	}
+	public static Supplier<String> addConstantString(String name, String value) {
+		SmartDashboard.putString(name, value);
+		return () -> SmartDashboard.getString(name);
 	}
 
 }
