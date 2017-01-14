1374f748a69d648c8d7b2736fd66489b0f647a32
ThinkRedstone <ittai11@gmail.com>
2017-01-14 10:46:36 +0200

    added default values

--- src/com/spikes2212/dashboard/ConstantHandler.java
+++ src/com/spikes2212/dashboard/ConstantHandler.java
@@ -7,16 +7,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class ConstantHandler {
 	public static Supplier<Double> addConstantDouble(String name, double value) {
 		SmartDashboard.putNumber(name, value);
-		return () -> SmartDashboard.getNumber(name);
+		return () -> SmartDashboard.getNumber(name,0);
 	}
 
 	public static Supplier<Integer> addConstantInt(String name, int value) {
 		SmartDashboard.putNumber(name, value);
-		return () -> (int) SmartDashboard.getNumber(name);
+		return () -> (int) SmartDashboard.getNumber(name, 0);
 	}
 	public static Supplier<String> addConstantString(String name, String value) {
 		SmartDashboard.putString(name, value);
-		return () -> SmartDashboard.getString(name);
+		return () -> SmartDashboard.getString(name, "");
 	}
 
 }
