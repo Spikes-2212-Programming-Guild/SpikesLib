0ce858220dbf7fe50a8e1452d2cdb08383f5672b
omri cohen <omrichoen@gmail.com>
2017-01-24 20:22:48 +0200

    fix the is finished

--- src/com/spikes2212/genericsubsystems/commands/MoveLimitedSubsystemWithPID.java
+++ src/com/spikes2212/genericsubsystems/commands/MoveLimitedSubsystemWithPID.java
@@ -3,6 +3,7 @@ package com.spikes2212.genericsubsystems.commands;
 import com.spikes2212.genericsubsystems.LimitedSubsystem;
 
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -13,9 +14,11 @@ public class MoveLimitedSubsystemWithPID extends Command {
 	private static double KP = 1;
 	private static double KI = 1;
 	private static double KD = 1;
+	private static double waitingTime = 1;
 	private static double tolerance = 1;
 	private PIDController movmentControl;
-	
+	private double lastFalseTime = 0;
+
 	public static void setP(double P) {
 		KP = P;
 	}
@@ -47,36 +50,51 @@ public class MoveLimitedSubsystemWithPID extends Command {
 	public static void setTolarance(double tolarance) {
 		tolerance = tolarance;
 	}
-	
+
+	public static void setWaitingTime(double newWaitingTime) {
+		waitingTime = newWaitingTime;
+	}
+
+	public static double getWaitingTime() {
+		return waitingTime;
+	}
+
 	public MoveLimitedSubsystemWithPID(double setPoint) {
 		movmentControl = new PIDController(KP, KI, KD, limitedSubsystem.getPIDSource(), limitedSubsystem::tryMove);
 		movmentControl.setAbsoluteTolerance(tolerance);
 		movmentControl.setSetpoint(setPoint);
 		movmentControl.setOutputRange(-1, 1);
 	}
-	
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	movmentControl.enable();
-    }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		movmentControl.enable();
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return movmentControl.onTarget();
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	movmentControl.disable();
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		double currentTime = Timer.getFPGATimestamp();
+		if (!movmentControl.onTarget()) {
+			lastFalseTime = currentTime;
+		}
+		if (currentTime - lastFalseTime > waitingTime) {
+			return true;
+		}
+		return false;
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		movmentControl.disable();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
