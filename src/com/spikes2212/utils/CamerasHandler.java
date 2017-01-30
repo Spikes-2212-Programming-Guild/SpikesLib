45d8d0a3c5d71853f7d26cee10a6e990e87f209e
ThinkRedstone <ittai11@gmail.com>
2017-01-30 21:43:20 +0200

    wrote documentation for CamerasHandler.java

--- src/com/spikes2212/utils/CamerasHandler.java
+++ src/com/spikes2212/utils/CamerasHandler.java
@@ -15,35 +15,62 @@ import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 
+/**
+ * This class handles the cameras used by the drivers,
+ * streaming the image from the chosen camera to the camera server under the name "CamerasHandler"
+ * and allowing you to switch between any amount of cameras.
+ *
+ * @author Omri "Riki" Cohen & Ittai "Dafner" Dafner
+ */
 public class CamerasHandler {
-	private int port;
-	private Map<Integer, CvSink> cvSinks = new HashMap<>();
+    private int port;
+    private Map<Integer, CvSink> cvSinks = new HashMap<>();
 
-	public CamerasHandler(int width, int height, int... ports) {
-		port = ports[0];
-		new Thread(() -> {
-			for (int i : ports) {
-				addCamera(i);
-			}
-			CvSource outputStream = CameraServer.getInstance().putVideo("CamerasHandler", width, height);
-			Mat frame = new Mat();
-			while (!Thread.interrupted()) {
-				cvSinks.get(port).grabFrame(frame); 
-				outputStream.putFrame(frame);
-			}
-		}).start();
-	}
+    /**
+     * Constructs a new instance of {@link CamerasHandler} that automatically starts streaming to the {@link CameraServer}
+     *
+     * @param width  The width the stream from the cameras should have, in pixels.
+     * @param height The height the stream from the cameras should have, in pixels.
+     * @param ports  The ports of the cameras this handler should start with.
+     */
+    public CamerasHandler(int width, int height, int... ports) {
+        port = ports[0];
+        new Thread(() -> {
+            for (int i : ports) {
+                addCamera(i);
+            }
+            CvSource outputStream = CameraServer.getInstance().putVideo("CamerasHandler", width, height);
+            Mat frame = new Mat();
+            while (!Thread.interrupted()) {
+                cvSinks.get(port).grabFrame(frame);
+                outputStream.putFrame(frame);
+            }
+        }).start();
+    }
 
-	public void switchCamera(int port) {
-		if (cvSinks.containsKey(port))
-			this.port = port;
-	}
+    /**
+     * Switches the stream to the stream from the camera in the given port,
+     * if that camera has been added to this handler.
+     * If no camera was added with that port, does nothing.
+     *
+     * @param port The port of the camera to switch the stream to
+     */
+    public void switchCamera(int port) {
+        if (cvSinks.containsKey(port))
+            this.port = port;
+    }
 
-	public void addCamera(int port) {
-		if (!cvSinks.containsKey(port)) {
-			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(port);
-			cvSinks.put(port, CameraServer.getInstance().getVideo(camera));
-		}
-	}
+    /**
+     * Adds a camera to this handler using the camera's port.
+     * If this handler already has a camera with that port, does nothing.
+     *
+     * @param port The port of the new camera to add.
+     */
+    public void addCamera(int port) {
+        if (!cvSinks.containsKey(port)) {
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(port);
+            cvSinks.put(port, CameraServer.getInstance().getVideo(camera));
+        }
+    }
 
 }
