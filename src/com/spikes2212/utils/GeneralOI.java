532c3dc003c16c32992bc3e0c4bb8bf2bc0d8e21
omri cohen <omrichoen@gmail.com>
2017-08-03 16:52:02 +0300

    added init methods

---
+++ src/com/spikes2212/utils/GeneralOI.java
@@ -0,0 +1,10 @@
+package com.spikes2212.utils;
+
+public abstract class GeneralOI {
+	
+	protected abstract void initDriverJoystick();
+	protected abstract void initNavigatorController();
+	
+	
+
+}
