a1df5c390dbc11ad722e1f927fcfa93944e7265f
omri cohen <omrichoen@gmail.com>
2017-08-03 16:54:26 +0300

    added value getting methods

--- src/com/spikes2212/utils/GeneralOI.java
+++ src/com/spikes2212/utils/GeneralOI.java
@@ -5,6 +5,8 @@ public abstract class GeneralOI {
 	protected abstract void initDriverJoystick();
 	protected abstract void initNavigatorController();
 	
-	
+	public abstract double getRotation();
+	public abstract double getForwardRight();
+	public abstract double getForwardLeft();
 
 }
