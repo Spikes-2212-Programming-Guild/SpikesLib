061548f77184e625783c8ddc8ffbcd19354276bb
omri cohen <omrichoen@gmail.com>
2017-08-10 18:41:22 +0300

    added turningSpeed parameter

--- src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrivetrain.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrivetrain.java
@@ -1,7 +1,7 @@
 package com.spikes2212.genericsubsystems.drivetrains;
 
 public abstract class HolonomicDrivetrain extends TankDrivetrain {
-	public abstract void holonomicDrive(double speedY, double speedX); // y-forward/backward
+	public abstract void holonomicDrive(double speedY, double speedX, double truningSpeed); // y-forward/backward
 	// x-left/right
 
 }
