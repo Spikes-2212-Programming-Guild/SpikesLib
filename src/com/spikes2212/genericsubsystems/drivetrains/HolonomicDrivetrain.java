7411f1ff8738672b809e55065fcb032202766dd8
omri cohen <omrichoen@gmail.com>
2017-08-10 17:47:56 +0300

    override the end method

--- src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrivetrain.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrivetrain.java
@@ -3,5 +3,10 @@ package com.spikes2212.genericsubsystems.drivetrains;
 public abstract class HolonomicDrivetrain extends TankDrivetrain {
 	public abstract void holonomicDrive(double speedY, double speedX, double turningSpeed); // y-forward/backward
 	// x-left/right
+	
+	@Override
+	public void stop(){
+    	holonomicDrive(0, 0, 0);
+    }
 
 }
