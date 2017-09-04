da29b128738e89c967137079cd1c2d396d6ba0fb
omri cohen <omrichoen@gmail.com>
2017-09-04 10:12:08 +0300

    updated PIDUsingDrivedrain to the new tankDrivetrain

--- src/com/spikes2212/genericsubsystems/drivetrains/PIDUsingTankDrivedrain.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/PIDUsingTankDrivedrain.java
@@ -1,25 +1,51 @@
 package com.spikes2212.genericsubsystems.drivetrains;
 
+import java.util.function.Consumer;
+
 import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public abstract class PIDUsingTankDrivedrain extends TankDrivetrain {
-	
+public class PIDUsingTankDrivedrain extends TankDrivetrain {
+
+	PIDSource leftPIDSource;
+	PIDSource rightPIDSource;
+
+	public PIDUsingTankDrivedrain(Consumer<Double> controlLeft, Consumer<Double> controlRight, Command defaultCommand,
+			PIDSource leftPIDSource, PIDSource rightPIDSource) {
+		super(controlLeft, controlRight, defaultCommand);
+		this.leftPIDSource = leftPIDSource;
+		this.rightPIDSource = rightPIDSource;
+
+	}
+
+	public PIDUsingTankDrivedrain(Consumer<Double> controlLeft, Consumer<Double> controlRight, PIDSource leftPIDSource,
+			PIDSource rightPIDSource) {
+		super(controlLeft, controlRight);
+		this.leftPIDSource = leftPIDSource;
+		this.rightPIDSource = rightPIDSource;
+
+	}
+
 	/**
-     * Configures and returns the default {@link PIDSource} for the left side of this drivetrain.
-     *
-     * @return the default {@link PIDSource} for the left side. This can be null if the subsystem supplies no default PID source.
-     */
-    public abstract PIDSource getLeftPIDSource();
+	 * Configures and returns the default {@link PIDSource} for the left side of
+	 * this drivetrain.
+	 *
+	 * @return the default {@link PIDSource} for the left side.
+	 */
+	public PIDSource getLeftPIDSource() {
+		return leftPIDSource;
+	}
 
-
-    /**
-     * Configures and returns the default {@link PIDSource} for the right side of this drivetrain.
-     *
-     * @return the default {@link PIDSource} for right side. This can be null if the subsystem supplies no default PID source.
-     */
-    public abstract PIDSource getRightPIDSource();
+	/**
+	 * Configures and returns the default {@link PIDSource} for the right side
+	 * of this drivetrain.
+	 *
+	 * @return the default {@link PIDSource} for right side.
+	 */
+	public PIDSource getRightPIDSource() {
+		return rightPIDSource;
+	}
 }
-
