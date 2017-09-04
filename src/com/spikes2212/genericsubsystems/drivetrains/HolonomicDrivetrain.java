fd39dfb2879bbfbda63ec05587e39f619a05c595
omri cohen <omrichoen@gmail.com>
2017-09-04 10:19:55 +0300

    updated holonomicDrivetrain to match drivetrian

--- src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrivetrain.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/HolonomicDrivetrain.java
@@ -1,7 +1,24 @@
 package com.spikes2212.genericsubsystems.drivetrains;
 
-public abstract class HolonomicDrivetrain extends TankDrivetrain {
-	public abstract void holonomicDrive(double speedY, double speedX); // y-forward/backward
-	// x-left/right
+import java.util.function.Consumer;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class HolonomicDrivetrain extends TankDrivetrain {
+	
+	Consumer<Double> controlX;
+	Consumer<Double> controlY;
+	
+	public HolonomicDrivetrain(Consumer<Double> controlLeft, Consumer<Double> controlRight, Command defaultCommand,
+			Consumer<Double> controlX, Consumer<Double> controlY) {
+		super(controlLeft, controlRight, defaultCommand);
+		this.controlX = controlX;
+		this.controlY = controlY;
+	}
+
+	public void holonomicDrive(double speedY, double speedX){ // y-forward/backward x-left/right
+		controlX.accept(speedX);
+		controlY.accept(speedY);
+	}
 
 }
