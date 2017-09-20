526893ced628e99c50ffe38dc85fb96f8b93014b
Darth Server <simon.kharmatsky@gmail.com>
2017-09-20 13:56:31 +0300

    added constructors to GyroTurn

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/GyroTurn.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/GyroTurn.java
@@ -1,11 +1,33 @@
 package com.spikes2212.genericsubsystems.drivetrains.commands;
 
+import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
 import edu.wpi.first.wpilibj.command.Command;
+
+import java.util.function.Supplier;
 
 public class GyroTurn extends Command {
 
+    private Supplier<Double> absoluteLeftSpeedSupplier;
+    private Supplier<Double> absoluteRightSpeedSupplier;
+    private Supplier<Double> targetDegreeSupplier;
+    private Supplier<Double> currentDegreeSupplier;
+
+    private TankDrivetrain drivetrain;
+    public GyroTurn(TankDrivetrain drivetrain,Supplier<Double> absoluteLeftSpeedSupplier, Supplier<Double> absoluteRightSpeedSupplier,
+                    Supplier<Double> targetDegreeSupplier, Supplier<Double> currentDegreeSupplier) {
+        this.drivetrain = drivetrain;
+        this.absoluteLeftSpeedSupplier = absoluteLeftSpeedSupplier;
+        this.absoluteRightSpeedSupplier = absoluteRightSpeedSupplier;
+        this.currentDegreeSupplier = currentDegreeSupplier;
+        this.targetDegreeSupplier = targetDegreeSupplier;
+    }
+
+    public GyroTurn(TankDrivetrain drivetrain, double absoluteLeftSpeed, double absoluteRightSpeed,
+                    double targetDegree, Supplier<Double> currentDegreeSupplier ) {
+        this(drivetrain, ()->absoluteLeftSpeed, ()->absoluteRightSpeed, ()->targetDegree, currentDegreeSupplier);
+    }
     @Override
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
 }
