b8326ee4bb829ead40d2f93effe9af5dea4776ac
Darth Server <simon.kharmatsky@gmail.com>
2017-09-20 15:15:21 +0300

    added rateSupplier field to GyroDrive

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/GyroDrive.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/GyroDrive.java
@@ -12,20 +12,20 @@ public class GyroDrive extends Command {
     private Supplier<Double> sensetivitySupplier =
             ConstantHandler.addConstantDouble("GyroDrive-Sensetivity", 0.5);
 
-    private Supplier<Double> speedSupplier;
+    private Supplier<Double> speedSupplier, rateSupplier;
     private TankDrivetrain drivetrain;
-    public GyroDrive(TankDrivetrain drivetrain, Supplier<Double> speedSupplier) {
+    public GyroDrive(TankDrivetrain drivetrain, Supplier<Double> speedSupplier, Supplier<Double> rateSupplier) {
         this.drivetrain = drivetrain;
         this.speedSupplier = speedSupplier;
+        this.rateSupplier = rateSupplier;
     }
 
-    public GyroDrive(TankDrivetrain drivetrain, double speed) {
-        this(drivetrain, ()->speed);
+    public GyroDrive(TankDrivetrain drivetrain, double speed, Supplier<Double> rateSupplier) {
+        this(drivetrain, ()->speed, rateSupplier);
     }
 
     @Override
     protected void execute() {
-
     }
 
     @Override
