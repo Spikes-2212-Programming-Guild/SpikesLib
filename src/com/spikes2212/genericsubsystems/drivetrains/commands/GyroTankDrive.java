b95945f9426433c8be8731af2569f311243020b0
Darth Server <simon.kharmatsky@gmail.com>
2017-09-20 15:47:00 +0300

    renamed GyroDrive to GyroTankDrive

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/GyroTankDrive.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/GyroTankDrive.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import java.util.function.Supplier;
 /**
  * This command is used to make {@link TankDrivetrain} drive on a particular angle.
- * By slowing one side of the drivetrain if {@link GyroDrive#rateSupplier} value is not 0.
+ * By slowing one side of the drivetrain if {@link GyroTankDrive#rateSupplier} value is not 0.
  * <p>
  *     This method implements an algorithm that was used in {@link RobotDrive#drive}
  *     to make the robot drive in arcade style by the given rate of the gyro.
@@ -20,38 +20,38 @@ import java.util.function.Supplier;
  * </p>
  * @author Simon "C" Kharmatsky
  */
-public class GyroDrive extends Command {
+public class GyroTankDrive extends Command {
     private Supplier<Double> sensetivitySupplier =
-            ConstantHandler.addConstantDouble("GyroDrive-Sensetivity", 0.5);
+            ConstantHandler.addConstantDouble("GyroTankDrive-Sensetivity", 0.5);
 
     private Supplier<Double> speedSupplier, rateSupplier;
     private TankDrivetrain drivetrain;
 
     /**
-     * This constructs a new {@link GyroDrive} command. using {@link TankDrivetrain} that the command will drive.
+     * This constructs a new {@link GyroTankDrive} command. using {@link TankDrivetrain} that the command will drive.
      * as well as {@link Supplier<Double>} instances for speed and rate.
      * @param drivetrain {@link TankDrivetrain} instance that the command is driving
      * @param speedSupplier {@link Supplier<Double>} that provides the speed the motors have to move at.
-     *                                              this value will be changed if {@link GyroDrive#rateSupplier}
+     *                                              this value will be changed if {@link GyroTankDrive#rateSupplier}
      *                                              returns a value that is not 0.
      * @param rateSupplier {@link Supplier<Double>} instance that provides the rate of the gyro
      */
-    public GyroDrive(TankDrivetrain drivetrain, Supplier<Double> speedSupplier, Supplier<Double> rateSupplier) {
+    public GyroTankDrive(TankDrivetrain drivetrain, Supplier<Double> speedSupplier, Supplier<Double> rateSupplier) {
         this.drivetrain = drivetrain;
         this.speedSupplier = speedSupplier;
         this.rateSupplier = rateSupplier;
     }
 
     /**
-     * This constructs a new {@link GyroDrive} command. using {@link TankDrivetrain},
+     * This constructs a new {@link GyroTankDrive} command. using {@link TankDrivetrain},
      * a static {@link Double} value for the speed and a {@link Supplier<Double>} instance for the rate of the gyro
      * @param drivetrain the drivetrain the command is driving
      * @param speed a static value for the speed the motors have to move at.
-     *              this value will be changed if {@link GyroDrive#rateSupplier}
+     *              this value will be changed if {@link GyroTankDrive#rateSupplier}
      *              returns a value that is not 0.
      * @param rateSupplier {@link Supplier<Double>} instance that provides the rate of the gyro
      */
-    public GyroDrive(TankDrivetrain drivetrain, double speed, Supplier<Double> rateSupplier) {
+    public GyroTankDrive(TankDrivetrain drivetrain, double speed, Supplier<Double> rateSupplier) {
         this(drivetrain, ()->speed, rateSupplier);
     }
 
