a1bd3a837a68049b6373ab0d8e3c669a421cde7a
Darth Server <simon.kharmatsky@gmail.com>
2017-09-20 13:50:35 +0300

    added GyroTurn class

---
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/GyroTurn.java
@@ -0,0 +1,11 @@
+package com.spikes2212.genericsubsystems.drivetrains.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class GyroTurn extends Command {
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
