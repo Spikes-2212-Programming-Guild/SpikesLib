bfc2428eadc0e01feb00918f2c1ebc4df843b17a
omri cohen <omrichoen@gmail.com>
2017-11-28 19:56:27 +0200

    added acceleration

--- src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemWithPIDForSpeed.java
+++ src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemWithPIDForSpeed.java
@@ -7,30 +7,33 @@ import com.spikes2212.genericsubsystems.BasicSubsystem;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDSource;
 
-public class MoveBasicSubsystemWithPIDForSpeed extends MoveBasicSubsystemWithPID{
+public class MoveBasicSubsystemWithPIDForSpeed extends MoveBasicSubsystemWithPID {
+
+	private double acceleration;
 
 	public MoveBasicSubsystemWithPIDForSpeed(BasicSubsystem basicSubsystem, PIDSource source, Supplier<Double> setpoint,
-			com.spikes2212.utils.PIDSettings PIDSettings) {
+			com.spikes2212.utils.PIDSettings PIDSettings, double acceleration) {
 		super(basicSubsystem, source, setpoint, PIDSettings);
+		this.acceleration = acceleration;
 	}
-	
+
 	public MoveBasicSubsystemWithPIDForSpeed(BasicSubsystem basicSubsystem, PIDSource source, double setpoint,
-			com.spikes2212.utils.PIDSettings PIDSettings) {
-		super(basicSubsystem, source, setpoint, PIDSettings);
+			com.spikes2212.utils.PIDSettings PIDSettings, double acceleration) {
+		this(basicSubsystem, source, () -> setpoint, PIDSettings, acceleration);
 	}
-	
+
 	@Override
 	protected void initialize() {
 		movmentControl = new PIDController(PIDSettings.getKP(), PIDSettings.getKI(), PIDSettings.getKD(), source,
-				basicSubsystem::addSpeed);
+				(additionalSpeed) -> basicSubsystem.addSpeed(additionalSpeed * acceleration));
 		movmentControl.setAbsoluteTolerance(PIDSettings.getTolerance());
 		movmentControl.setSetpoint(this.setpoint.get());
 		movmentControl.setOutputRange(-1, 1);
 		movmentControl.enable();
 	}
-	
+
 	@Override
-	protected boolean isFinished(){
+	protected boolean isFinished() {
 		return false;
 	}
 
