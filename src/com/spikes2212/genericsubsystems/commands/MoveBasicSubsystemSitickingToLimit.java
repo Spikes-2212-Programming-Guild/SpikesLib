72c63e887a7c3ab780a11b40a0558b70fec1875d
omri cohen <omrichoen@gmail.com>
2018-01-11 18:25:18 +0200

    overrided isFinished

--- src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemSitickingToLimit.java
+++ src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemSitickingToLimit.java
@@ -4,14 +4,32 @@ import java.util.function.Supplier;
 
 import com.spikes2212.genericsubsystems.BasicSubsystem;
 
-public class MoveBasicSubsystemSitickingToLimit extends MoveBasicSubsystem {
+import edu.wpi.first.wpilibj.Timer;
 
-	public MoveBasicSubsystemSitickingToLimit(BasicSubsystem basicSubsystem, Supplier<Double> speed) {
+public class MoveBasicSubsystemSitickingToLimit extends MoveBasicSubsystem {
+	private double waitTime;
+	private double lastFalseTime = 0;
+
+	public MoveBasicSubsystemSitickingToLimit(BasicSubsystem basicSubsystem, Supplier<Double> speed, double waitTime) {
 		super(basicSubsystem, speed);
+		this.waitTime = waitTime;
 	}
-	
-	public MoveBasicSubsystemSitickingToLimit(BasicSubsystem basicSubsystem, double speed) {
+
+	public MoveBasicSubsystemSitickingToLimit(BasicSubsystem basicSubsystem, double speed, double waitTime) {
 		super(basicSubsystem, speed);
+		this.waitTime = waitTime;
+	}
+
+	@Override
+	protected boolean isFinished() {
+		double currentTime = Timer.getMatchTime();
+		if (!super.isFinished()) {
+			lastFalseTime = currentTime;
+		}
+		if (currentTime - lastFalseTime > waitTime) {
+			return true;
+		}
+		return false;
 	}
 
 }
