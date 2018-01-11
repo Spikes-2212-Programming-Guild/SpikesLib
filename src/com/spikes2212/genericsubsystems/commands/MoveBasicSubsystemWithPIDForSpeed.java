80e158d46ddb297797c1ea9971b875e4a5f4a11f
omri cohen <omrichoen@gmail.com>
2018-01-11 17:28:04 +0200

    added docs

--- src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemWithPIDForSpeed.java
+++ src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemWithPIDForSpeed.java
@@ -3,23 +3,67 @@ package com.spikes2212.genericsubsystems.commands;
 import java.util.function.Supplier;
 
 import com.spikes2212.genericsubsystems.BasicSubsystem;
+import com.spikes2212.utils.PIDSettings;
 
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDSource;
 
+/**
+ * This command makes a {@link BasicSubsystem} using wpilib's
+ * {@link PIDController} with a given speed and not a given voltage. After
+ * reaching the wanted speed it sticks.
+ *
+ * @author Omri "Riki"
+ * @see BasicSubsystem
+ * @see MoveBasicSubsystemWithPID
+ * @see PIDController
+ * @see PIDSettings
+ */
 public class MoveBasicSubsystemWithPIDForSpeed extends MoveBasicSubsystemWithPID {
 
 	private double acceleration;
 
-	public MoveBasicSubsystemWithPIDForSpeed(BasicSubsystem basicSubsystem, PIDSource source, Supplier<Double> setpoint,
-			com.spikes2212.utils.PIDSettings PIDSettings, double acceleration) {
-		super(basicSubsystem, source, setpoint, PIDSettings);
+	/**
+	 * This constructs a new {@link MoveBasicSubsystemWithPIDForSpeed} using a
+	 * {@link PIDSource}, the wanted speed, the PID coefficients this command's PID
+	 * loop should have, and the tolerance for error.
+	 *
+	 * @param basicSubsystem
+	 *            the {@link BasicSubsystem} this command requires and moves.
+	 * @param source
+	 *            the {@link PIDSource} this command uses to get feedback for
+	 *            the PID Loop.
+	 * @param wantedSpeed
+	 *            a supplier supplying the speed the {@link BasicSubsystem} should move at.
+	 * @param PIDSettings
+	 *            the {@link PIDSettings} this command's PIDController needs.
+	 * @see PIDController
+	 */
+	public MoveBasicSubsystemWithPIDForSpeed(BasicSubsystem basicSubsystem, PIDSource source, Supplier<Double> wantedSpeed,
+			PIDSettings PIDSettings, double acceleration) {
+		super(basicSubsystem, source, wantedSpeed, PIDSettings);
 		this.acceleration = acceleration;
 	}
 
-	public MoveBasicSubsystemWithPIDForSpeed(BasicSubsystem basicSubsystem, PIDSource source, double setpoint,
-			com.spikes2212.utils.PIDSettings PIDSettings, double acceleration) {
-		this(basicSubsystem, source, () -> setpoint, PIDSettings, acceleration);
+	/**
+	 * This constructs a new {@link MoveBasicSubsystemWithPIDForSpeed} using a
+	 * {@link PIDSource}, the wanted speed, the PID coefficients this command's PID
+	 * loop should have, and the tolerance for error.
+	 *
+	 * @param basicSubsystem
+	 *            the {@link BasicSubsystem} this command requires and moves.
+	 * @param source
+	 *            the {@link PIDSource} this command uses to get feedback for
+	 *            the PID Loop.
+	 * @param wantedSpeed
+	 *            the speed the {@link BasicSubsystem} should move at.
+	 * @param PIDSettings
+	 *            the {@link PIDSettings} this command's PIDController needs.
+	 * @see PIDController
+	 */
+	public MoveBasicSubsystemWithPIDForSpeed(BasicSubsystem basicSubsystem, PIDSource source, double wantedSpeed,
+			PIDSettings PIDSettings, double acceleration) {
+		this(basicSubsystem, source, () -> wantedSpeed, PIDSettings, acceleration);
 	}
 
 	@Override
@@ -34,7 +78,7 @@ public class MoveBasicSubsystemWithPIDForSpeed extends MoveBasicSubsystemWithPID
 
 	@Override
 	protected boolean isFinished() {
-		return false;
+		return false; //The subsystem should not stop when reaching the wanted speed. 
 	}
 
 }
