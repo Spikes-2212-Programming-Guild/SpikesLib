7fbd20c899b6458de1226b5b5340d28466e02435
omri cohen <omrichoen@gmail.com>
2018-01-11 18:32:31 +0200

    added docs

--- src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemSitickingToLimit.java
+++ src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemSitickingToLimit.java
@@ -6,15 +6,55 @@ import com.spikes2212.genericsubsystems.BasicSubsystem;
 
 import edu.wpi.first.wpilibj.Timer;
 
+/**
+ * This command moves a {@link BasicSubsystem} according to a {@link Supplier}
+ * or a constant speed until it cannot move no longer and then wait a given
+ * number of second to make the {@link BasicSubsystem} stick to its end state.
+ * 
+ *
+ * @author Omri "Riki" Cohen
+ * @see BasicSubsystem
+ */
 public class MoveBasicSubsystemSitickingToLimit extends MoveBasicSubsystem {
 	private double waitTime;
 	private double lastFalseTime = 0;
 
+	/**
+	 * This constructs a new {@link MoveBasicSubsystemSitickingToLimit} command
+	 * using the {@link BasicSubsystem} this command runs on and the supplier
+	 * supplying the speed the {@link BasicSubsystem} should be moved with and a
+	 * wait time.
+	 *
+	 * @param basicSubsystem
+	 *            the {@link BasicSubsystem} this command should move.
+	 * @param speedSupplier
+	 *            a Double {@link Supplier} supplying the speed basicSubsystem
+	 *            should be moved with. Should only supply values between -1 and
+	 *            1.
+	 * @param waitTime
+	 *            the time the {@link BasicSubsystem} should keep trying to move
+	 *            since reaching its end point.
+	 */
 	public MoveBasicSubsystemSitickingToLimit(BasicSubsystem basicSubsystem, Supplier<Double> speed, double waitTime) {
 		super(basicSubsystem, speed);
 		this.waitTime = waitTime;
 	}
 
+	/**
+	 * This constructs a new {@link MoveBasicSubsystemSitickingToLimit} command
+	 * using the {@link BasicSubsystem} this command runs on and the supplier
+	 * supplying the speed the {@link BasicSubsystem} should be moved with and a
+	 * wait time.
+	 *
+	 * @param basicSubsystem
+	 *            the {@link BasicSubsystem} this command should move.
+	 * @param speed
+	 *            the speed {@link BasicSubsystem} should be moved with. Should only
+	 *            be values between -1 and 1.
+	 * @param waitTime
+	 *            the time the {@link BasicSubsystem} should keep trying to move
+	 *            since reaching its end point.
+	 */
 	public MoveBasicSubsystemSitickingToLimit(BasicSubsystem basicSubsystem, double speed, double waitTime) {
 		super(basicSubsystem, speed);
 		this.waitTime = waitTime;
@@ -24,7 +64,7 @@ public class MoveBasicSubsystemSitickingToLimit extends MoveBasicSubsystem {
 	protected boolean isFinished() {
 		double currentTime = Timer.getMatchTime();
 		if (!super.isFinished()) {
-			lastFalseTime = currentTime;
+			lastFalseTime = currentTime; 
 		}
 		if (currentTime - lastFalseTime > waitTime) {
 			return true;
