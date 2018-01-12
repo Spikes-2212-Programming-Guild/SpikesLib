7cfaa3d1611e2a046c4333d8119340359cfb60f9
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-01-12 14:49:17 +0000

    implemented methods from Command in DriveTankByGyroWithPID

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveTankByGyroWithPID.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveTankByGyroWithPID.java
@@ -3,6 +3,7 @@ package com.spikes2212.genericsubsystems.drivetrains.commands;
 import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
 import com.spikes2212.utils.PIDSettings;
 import edu.wpi.first.wpilibj.GyroBase;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -16,7 +17,7 @@ public class DriveTankByGyroWithPID extends Command{
     private Supplier<Double> arcadeSpeedSupplier;
     private PIDSettings settings;
 
-
+    private PIDController angleController;
 
     public DriveTankByGyroWithPID(TankDrivetrain drivetrain, GyroBase source, Supplier<Double> setpointSupplier,
                                   Supplier<Double> arcadeSpeedSupplier, PIDSettings settings) {
@@ -34,6 +35,35 @@ public class DriveTankByGyroWithPID extends Command{
         this.setpointSupplier = () -> setpoint;
         this.arcadeSpeedSupplier = () -> arcadeSpeed;
         this.settings = settings;
+    }
+
+    @Override
+    protected void initialize() {
+        this.angleController = new PIDController(settings.getKP(), settings.getKI(), settings.getKD(), source,
+                (toggle) -> drivetrain.tankDrive(arcadeSpeedSupplier.get(), toggle));
+        angleController.setAbsoluteTolerance(settings.getTolerance());
+        angleController.setSetpoint(setpointSupplier.get());
+        angleController.setOutputRange(-1.0, 0.1);
+        angleController.enable();
+    }
+
+    @Override
+    protected void execute() {
+        double newSetpoint = setpointSupplier.get();
+        if (angleController.getSetpoint() != newSetpoint) {
+            angleController.setSetpoint(newSetpoint);
+        }
+    }
+
+    @Override
+    protected void end() {
+        angleController.disable();
+        drivetrain.stop();
+    }
+
+    @Override
+    protected void interrupted() {
+        end();
     }
 
     @Override
