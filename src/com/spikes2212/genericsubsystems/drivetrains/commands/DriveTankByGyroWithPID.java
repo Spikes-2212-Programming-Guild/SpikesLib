e9e503a52960950c30af00c3ad362957524de805
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-01-13 11:17:06 +0000

    added new constructors and javadoc for them

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveTankByGyroWithPID.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveTankByGyroWithPID.java
@@ -25,6 +25,7 @@ public class DriveTankByGyroWithPID extends Command{
     private PIDSource source;
     private Supplier<Double> setpointSupplier;
     private Supplier<Double> speedSupplier;
+    private Supplier<Boolean> isFinishedSupplier;
     private PIDSettings settings;
 
     private PIDController angleController;
@@ -39,15 +40,17 @@ public class DriveTankByGyroWithPID extends Command{
      * @param setpointSupplier {@link Supplier<Double>} for the degree the robot has to be at
      * @param speedSupplier {@link Supplier<Double>} supplier of the speed for
      *                                              {@link TankDrivetrain#arcadeDrive}
+     * @param isFinishedSupplier {@link Supplier<Boolean>} that checks if the command is finished
      * @param settings {@link PIDSettings} for this command
      */
     public DriveTankByGyroWithPID(TankDrivetrain drivetrain, GyroBase source, Supplier<Double> setpointSupplier,
-                                  Supplier<Double> speedSupplier, PIDSettings settings) {
+                                  Supplier<Double> speedSupplier, Supplier<Boolean> isFinishedSupplier, PIDSettings settings) {
         requires(drivetrain);
         this.drivetrain = drivetrain;
         this.source = source;
         this.setpointSupplier = setpointSupplier;
         this.speedSupplier = speedSupplier;
+        this.isFinishedSupplier = isFinishedSupplier;
         this.settings = settings;
     }
 
@@ -57,14 +60,50 @@ public class DriveTankByGyroWithPID extends Command{
      * {@link DriveTankByGyroWithPID#speedSupplier} instead of {@link Supplier<Double>}s
      * @param drivetrain the {@link DriveTankByGyroWithPID} this command requires and moves
      * @param source the {@link PIDSource} that this command uses to get feedback about the
-     *               {@link DriveTankByGyroWithPID}'s degree
+     *               {@link DriveTankByGyroWithPID#drivetrain}'s degree
      * @param setpoint static value for {@link DriveTankByGyroWithPID#setpointSupplier}
      * @param speed static value for {@link DriveTankByGyroWithPID#speedSupplier}
+     * @param isFinishedSupplier {@link Supplier<Boolean>} that checks if the command is finished
      * @param settings @link PIDSettings} for this command
      */
     public DriveTankByGyroWithPID(TankDrivetrain drivetrain, GyroBase source,
-                                  double setpoint, double speed, PIDSettings settings) {
-        this(drivetrain, source, () -> setpoint, () -> speed, settings);
+                                  double setpoint, double speed, Supplier<Boolean> isFinishedSupplier, PIDSettings settings) {
+        this(drivetrain, source, () -> setpoint, () -> speed, isFinishedSupplier, settings);
+    }
+
+    /**
+     * This constructs a new {@link DriveTankByGyroWithPID}
+     * ignoring the {@link DriveTankByGyroWithPID#isFinishedSupplier}
+     * @param drivetrain the {@link DriveTankByGyroWithPID} this command requires and moves
+     *
+     * @param source the {@link PIDSource} that this command uses to get feedback about the
+     *               {@link DriveTankByGyroWithPID#drivetrain}'s degree
+     * @param setpointSupplier {@link Supplier<Double>} for the degree the robot has to be at
+     * @param speedSupplier supplier of the speed for
+     *                                              {@link TankDrivetrain#arcadeDrive}
+     * @param settings {@link PIDSettings} for this command
+     */
+    public DriveTankByGyroWithPID(TankDrivetrain drivetrain, GyroBase source, Supplier<Double> setpointSupplier,
+                                  Supplier<Double> speedSupplier, PIDSettings settings) {
+        this(drivetrain, source, setpointSupplier, speedSupplier, () -> false , settings);
+
+    }
+
+    /**
+     * This constructs a new {@link DriveTankByGyroWithPID}
+     * ignoring the {@link DriveTankByGyroWithPID#isFinishedSupplier} and uses
+     * constant values for {@link DriveTankByGyroWithPID#setpointSupplier}
+     * and {@link DriveTankByGyroWithPID#speedSupplier}
+     * @param drivetrain the {@link DriveTankByGyroWithPID} this command requires and moves
+     * @param source the {@link PIDSource} that this command uses to get feedback about the
+     *               {@link DriveTankByGyroWithPID#drivetrain}'s degree
+     * @param setpoint constant value for {@link DriveTankByGyroWithPID#setpointSupplier}
+     * @param speed constant value for {@link DriveTankByGyroWithPID#speedSupplier}
+     * @param settings {@link PIDSettings} for this command
+     */
+    public DriveTankByGyroWithPID(TankDrivetrain drivetrain, GyroBase source, double setpoint,
+                                  double speed, PIDSettings settings) {
+        this(drivetrain, source, ()-> setpoint, ()->speed, settings);
     }
 
     @Override
@@ -98,8 +137,6 @@ public class DriveTankByGyroWithPID extends Command{
 
     @Override
     protected boolean isFinished() {
-        return isTimedOut();
+        return isTimedOut() || isFinishedSupplier.get();
     }
-
-
 }
