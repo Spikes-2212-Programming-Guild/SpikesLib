ffe8042b4552a74948469f0c9c96427bb8e90607
omri cohen <omrichoen@gmail.com>
2018-01-14 17:51:47 +0200

    added NumericalTarget

---
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/targetSuppliers/NumericalTarget.java
@@ -0,0 +1,21 @@
+package com.spikes2212.genericsubsystems.drivetrains.commands.targetSuppliers;
+
+import java.util.function.Supplier;
+
+public class NumericalTarget implements Supplier<Boolean> {
+
+	protected final Supplier<Double> position;
+	private double targetPosition, tolerance;
+
+	public NumericalTarget(Supplier<Double> position, double targetPosition, double tolerance) {
+		this.position = position;
+		this.targetPosition = targetPosition;
+		this.tolerance = tolerance;
+	}
+
+	@Override
+	public Boolean get() {
+		return targetPosition - tolerance <= position.get() && position.get() <= targetPosition + tolerance;
+	}
+
+}
