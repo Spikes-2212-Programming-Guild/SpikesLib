2c15fcd99db1958878e862f2b5d39893d2ad5203
omri cohen <omrichoen@gmail.com>
2018-01-14 17:32:25 +0200

    made MoveBasicSubsystemToTarget

---
+++ src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemToTarget.java
@@ -0,0 +1,19 @@
+package com.spikes2212.genericsubsystems.commands;
+
+import java.util.function.Supplier;
+
+import com.spikes2212.genericsubsystems.BasicSubsystem;
+
+public class MoveBasicSubsystemToTarget extends MoveBasicSubsystem {
+	protected Supplier<Boolean> onTarget;
+
+	public MoveBasicSubsystemToTarget(BasicSubsystem basicSubsystem, Supplier<Double> speedSupplier,
+			Supplier<Boolean> onTarget) {
+		super(basicSubsystem, speedSupplier);
+		this.onTarget = onTarget;
+	}
+
+	protected boolean isFinished() {
+		return onTarget.get() || super.isFinished();
+	}
+}
