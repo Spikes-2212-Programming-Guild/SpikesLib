3a31ab907ad3f45ac3b1dc284c768c2963d449f1
Yonatan <yognatan33@gmail.com>
2018-01-16 12:10:40 +0200

    Finished all normal drivetrain commands
    Arcade, Tank and Holonomic

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveArcade.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveArcade.java
@@ -7,87 +7,94 @@ import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- * This command moves a {@link TankDrivetrain} using the arcade control method used written by WPILIB.
- *
- * @author Unknown
+ * This command moves a {@link TankDrivetrain} using linear and rotational
+ * speeds.
  */
 public class DriveArcade extends Command {
 	protected final TankDrivetrain tankDrivetrain;
 	protected final Supplier<Double> moveValueSupplier;
 	protected final Supplier<Double> rotateValueSupplier;
 
-    /**
-     * This constructs a new {@link DriveArcade} command
-     * that moves the given {@link TankDrivetrain} forward while also turning according the WPILIB's arcade drive.
-     *
-     * @param drivetrain  the drivetrain this command requires and moves.
-     * @param moveValue   the speed to move forward with. Negative values go backwards.
-     * @param rotateValue the speed to turn with. Negative values turn right.
-     */
-    public DriveArcade(TankDrivetrain drivetrain, double moveValue, double rotateValue) {
-        this(drivetrain, () -> moveValue, () -> rotateValue);
-    }
+	/**
+	 * This constructs a new {@link DriveArcade} command that moves the given
+	 * {@link TankDrivetrain} acording to constant linear and rotational speeds.
+	 *
+	 * @param drivetrain
+	 *            the tank drivetrain this command opperates on.
+	 * @param moveValue
+	 *            the speed to move straight with. Positive values go forwards.
+	 * @param rotateValue
+	 *            the speed to turn with. Positive values turn left.
+	 */
+	public DriveArcade(TankDrivetrain drivetrain, double moveValue, double rotateValue) {
+		this(drivetrain, () -> moveValue, () -> rotateValue);
+	}
 
-    /**
-     * This constructs a new {@link DriveArcade} command
-     * that moves the given {@link TankDrivetrain} forward while also turning using changing speeds according the WPILIB's arcade drive.
-     *
-     * @param drivetrain  the drivetrain this command requires and moves.
-     * @param moveValueSupplier   the double {@link Supplier} supplying the speed to move forward with. Negative values go backwards.
-     * @param rotateValueSupplier the double {@link Supplier} supplying the speed to turn with. Negative values go right.
-     */
-    public DriveArcade(TankDrivetrain drivetrain, Supplier<Double> moveValueSupplier, Supplier<Double> rotateValueSupplier) {
-        requires(drivetrain);
-        this.tankDrivetrain = drivetrain;
-        this.moveValueSupplier = moveValueSupplier;
-        this.rotateValueSupplier = rotateValueSupplier;
-    }
+	/**
+	 * This constructs a new {@link DriveArcade} command that moves the given
+	 * {@link TankDrivetrain} acording to speed values from Double {@link Supplier}s
+	 * for linear and rotational movments.
+	 *
+	 * @param drivetrain
+	 *            the tank drivetrain this command opperates on.
+	 * @param moveValueSupplier
+	 *            the double {@link Supplier} supplying the speed to move forward
+	 *            with. Positive values go forwards.
+	 * @param rotateValueSupplier
+	 *            the double {@link Supplier} supplying the speed to turn with.
+	 *            Positive values go left.
+	 */
+	public DriveArcade(TankDrivetrain drivetrain, Supplier<Double> moveValueSupplier,
+			Supplier<Double> rotateValueSupplier) {
+		requires(drivetrain);
+		this.tankDrivetrain = drivetrain;
+		this.moveValueSupplier = moveValueSupplier;
+		this.rotateValueSupplier = rotateValueSupplier;
+	}
 
-    @Override
-    protected void initialize() {
-        // TODO Auto-generated method stub
+	@Override
+	protected void initialize() {
 
-    }
+	}
 
-    @Override
-    protected void execute() {
-        double leftSpeed, rightSpeed;
-        double moveValue = moveValueSupplier.get();
-        double rotateValue = rotateValueSupplier.get();
-        if (moveValue > 0.0) {
-            if (rotateValue > 0.0) {
-                leftSpeed = moveValue - rotateValue;
-                rightSpeed = Math.max(moveValue, rotateValue);
-            } else {
-                leftSpeed = Math.max(moveValue, -rotateValue);
-                rightSpeed = moveValue + rotateValue;
-            }
-        } else {
-            if (rotateValue > 0.0) {
-                leftSpeed = -Math.max(-moveValue, rotateValue);
-                rightSpeed = moveValue + rotateValue;
-            } else {
-                leftSpeed = moveValue - rotateValue;
-                rightSpeed = -Math.max(-moveValue, -rotateValue);
-            }
-        }
-        tankDrivetrain.tankDrive(leftSpeed, rightSpeed);
-    }
+	@Override
+	protected void execute() {
+		double leftSpeed, rightSpeed;
+		double moveValue = moveValueSupplier.get();
+		double rotateValue = rotateValueSupplier.get();
+		if (moveValue > 0.0) {
+			if (rotateValue > 0.0) {
+				leftSpeed = moveValue - rotateValue;
+				rightSpeed = Math.max(moveValue, rotateValue);
+			} else {
+				leftSpeed = Math.max(moveValue, -rotateValue);
+				rightSpeed = moveValue + rotateValue;
+			}
+		} else {
+			if (rotateValue > 0.0) {
+				leftSpeed = -Math.max(-moveValue, rotateValue);
+				rightSpeed = moveValue + rotateValue;
+			} else {
+				leftSpeed = moveValue - rotateValue;
+				rightSpeed = -Math.max(-moveValue, -rotateValue);
+			}
+		}
+		tankDrivetrain.tankDrive(leftSpeed, rightSpeed);
+	}
 
-    @Override
-    protected boolean isFinished() {
-        // TODO Auto-generated method stub
-        return isTimedOut();
-    }
+	@Override
+	protected boolean isFinished() {
+		return isTimedOut();
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-        tankDrivetrain.tankDrive(0, 0);
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		tankDrivetrain.stop();
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-        end();
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
