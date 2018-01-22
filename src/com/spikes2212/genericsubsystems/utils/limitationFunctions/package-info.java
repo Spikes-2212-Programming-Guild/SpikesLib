87c971b0bf174044481ec0b970c94a7418affcb4
tomer sitton <tomer.sitton1@gmail.com>
2018-01-22 12:48:08 +0200

    added package-info.java to package genericsubsystems.utils.limitationFunctions.

---
+++ src/com/spikes2212/genericsubsystems/utils/limitationFunctions/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * com.spikes2212.genericsubsystems.utils.limitationFunctions contains limitation classes which can be used when constructing a new {@link com.spikes2212.genericsubsystems.BasicSubsystem}.
+ * These classes extends from Function<Double,Boolean>, and thus should be used as the {@link com.spikes2212.genericsubsystems.BasicSubsystem}'s "canMove" function.
+ */
+package com.spikes2212.genericsubsystems.utils.limitationFunctions;
