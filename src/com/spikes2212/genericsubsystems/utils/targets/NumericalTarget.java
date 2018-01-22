2190c5023ccdbd3bb7689c8ee0d9c6e23df7bd20
tomer sitton <tomer.sitton1@gmail.com>
2018-01-22 12:33:42 +0200

    moved NumericalTarget.java to package genericSubsystems.utils.targets

---
+++ src/com/spikes2212/genericsubsystems/utils/targets/NumericalTarget.java
@@ -0,0 +1,21 @@
+package com.spikes2212.genericsubsystems.utils.targets;
+
+import java.util.function.Supplier;
+
+public class NumericalTarget implements Supplier<Boolean> {
+
+	protected final Supplier<Double> position;
+	private double targetPosition, tolerance;
+
+	public NumericalTarget(Supplier<Double> position, double targetPosition, double tolerance) {
+		this.position = position;
+		this.targetPosition = targetPosition;
+		this.tolerance = tolerance;
+	}
+
+	@Override
+	public Boolean get() {
+		return (Math.abs(targetPosition - position.get()) <= tolerance);
+	}
+
+}
