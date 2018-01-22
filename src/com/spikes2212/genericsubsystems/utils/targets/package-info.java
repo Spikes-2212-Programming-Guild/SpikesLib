0b0f9815e7acb2ad7003d84fb53c788818e7acb0
tomer sitton <tomer.sitton1@gmail.com>
2018-01-22 12:54:50 +0200

    added package-info.java to genericsubsystems.utils.targets package

---
+++ src/com/spikes2212/genericsubsystems/utils/targets/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * com.spikes2212.genericsubsystems.utils.targets contains classes which represents generic targets implementations which can be used as the "target" of a {@link com.spikes2212.genericsubsystems.commands.MoveBasicSubsystemToTarget} command.
+ * For example - a numerical target with a certain tolerance. The info of the current state can be provided from any numerical sensor.
+ */
+package com.spikes2212.genericsubsystems.utils.targets;
