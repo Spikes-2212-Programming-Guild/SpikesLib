e8b5b04b73ec2458076c5e77a95d1eb3d9f197c1
Matangn13 <matangn13@gmail.com>
2018-01-25 18:36:39 +0200

    added constructors

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/TurnToGlobalAngleWithPID.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/TurnToGlobalAngleWithPID.java
@@ -1,5 +1,22 @@
 package com.spikes2212.genericsubsystems.drivetrains.commands;
 
+import java.util.function.Supplier;
+
+import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
+import com.spikes2212.utils.PIDSettings;
+
+import edu.wpi.first.wpilibj.PIDSource;
+
 public class TurnToGlobalAngleWithPID extends TurnToRelativeAngleWithPID{
+
+	public TurnToGlobalAngleWithPID(TankDrivetrain drivetrain, PIDSource source, Supplier<Double> setpointSupplier,
+			PIDSettings settings) {
+		super(drivetrain, source, () -> setpointSupplier.get() - source.pidGet(), settings);
+	}
+	
+	public TurnToGlobalAngleWithPID(TankDrivetrain drivetrain, PIDSource source, double setpoint,
+			PIDSettings settings) {
+		this(drivetrain, source, () -> setpoint, settings);
+	}
 
 }
