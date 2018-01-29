91fc54e4cfee7f818cdba7760ce37a3f0233a44f
Spikes-2212 <spikes.no2212@gmail.com>
2018-01-29 21:03:42 +0200

    now not updating the starting value

--- src/com/spikes2212/genericsubsystems/drivetrains/utils/RelativeAangularTarget.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/utils/RelativeAangularTarget.java
@@ -3,13 +3,16 @@ package com.spikes2212.genericsubsystems.drivetrains.utils;
 import java.util.function.Supplier;
 
 public class RelativeAangularTarget extends GlobalAangularTarget {
+	
+	private RelativeAangularTarget(Supplier<Double> targetSupplier, Supplier<Double> positionSupplier, double startingPosition) {
+		super(() -> targetSupplier.get() + startingPosition, positionSupplier);
+	}
 
 	public RelativeAangularTarget(Supplier<Double> targetSupplier, Supplier<Double> positionSupplier) {
-		super(() -> targetSupplier.get() + positionSupplier.get(), positionSupplier);
+		this(targetSupplier, positionSupplier, positionSupplier.get());
 	}
 	
 	public RelativeAangularTarget(double target, Supplier<Double> positionSupplier){
 		this(() -> target, positionSupplier);
 	}
-
 }
