6fabe12b644f5587bcff9647cec10dd144a4d1d0
omri cohen <omrichoen@gmail.com>
2018-01-31 21:03:49 +0200

    add new docs to MonitoredDriveArcade

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/MonitoredDriveArcade.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/MonitoredDriveArcade.java
@@ -6,41 +6,60 @@ import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
 import com.spikes2212.utils.VoltageMonitor;
 
 /**
- * This command work similarly to {@link DriveArcade},
- * only it scales down the speed given to the {@link TankDrivetrain} according to the current voltage.
- * <p>
- * The supplier is scaled according to {@link VoltageMonitor#monitorSupplier(Supplier)},
- * meaning according to the default scaling method and values in {@link VoltageMonitor}.
- * </p>
+ * This command work similarly to {@link DriveArcade}, only it scales down the
+ * speed given to the {@link TankDrivetrain} according to the current voltage.
+ * 
+ * 
+ * <br>
+ * <br>
+ * The supplier is scaled according to
+ * {@link VoltageMonitor#monitorSupplier(Supplier)}, meaning according to the
+ * default scaling method and values in {@link VoltageMonitor}.
  *
  * @author Omri "Riki" Cohen
- * @see DriveArcade
+ * @see DriveTank
  * @see VoltageMonitor
  */
 public class MonitoredDriveArcade extends DriveArcade {
 
+	/**
+	 * This constructs a new {@link DriveArcade} command that moves the given
+	 * {@link TankDrivetrain} acording to speed values from Double
+	 * {@link Supplier}s for linear and rotational movments.
+	 *
+	 * @param drivetrain
+	 *            the tank drivetrain this command opperates on.
+	 * @param moveValueSupplier
+	 *            the double {@link Supplier} supplying the speed to move
+	 *            forward with. Positive values go forwards.
+	 * @param rotateValueSupplier
+	 *            the double {@link Supplier} supplying the speed to turn with.
+	 *            Positive values go left.
+	 * 
+	 * @see VoltageMonitor#monitorSupplier(Supplier)
+	 */
+	public MonitoredDriveArcade(TankDrivetrain drivetrain, Supplier<Double> moveValueSupplier,
+			Supplier<Double> rotateValueSupplier) {
+		super(drivetrain, VoltageMonitor.monitorSupplier(moveValueSupplier),
+				VoltageMonitor.monitorSupplier(rotateValueSupplier));
+	}
 
-    /**
-     * {@inheritDoc}
-     *
-     * @param moveValueSupplier   {@inheritDoc} Values from this supplier are scaled using {@link VoltageMonitor#monitorSupplier(Supplier)}.
-     * @param rotateValueSupplier {@inheritDoc} Values from this supplier are scaled using {@link VoltageMonitor#monitorSupplier(Supplier)}.
-     */
-    public MonitoredDriveArcade(TankDrivetrain drivetrain, Supplier<Double> moveValueSupplier,
-                                Supplier<Double> rotateValueSupplier) {
-        super(drivetrain, VoltageMonitor.monitorSupplier(moveValueSupplier),
-                VoltageMonitor.monitorSupplier(rotateValueSupplier));
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @param moveValue   {@inheritDoc} This speed is dynamically scaled down according to {@link VoltageMonitor#monitorSupplier(Supplier)}.
-     * @param rotateValue {@inheritDoc} This speed is dynamically scaled down according to {@link VoltageMonitor#monitorSupplier(Supplier)}.
-     */
-    public MonitoredDriveArcade(TankDrivetrain drivetrain, double moveValue, double rotateValue) {
-        super(drivetrain, VoltageMonitor.monitorSupplier(() -> moveValue),
-                VoltageMonitor.monitorSupplier(() -> rotateValue));
-    }
+	/**
+	 * This constructs a new {@link DriveArcade} command that moves the given
+	 * {@link TankDrivetrain} acording to constant linear and rotational speeds.
+	 *
+	 * @param drivetrain
+	 *            the tank drivetrain this command opperates on.
+	 * @param moveValue
+	 *            the speed to move straight with. Positive values go forwards.
+	 * @param rotateValue
+	 *            the speed to turn with. Positive values turn left.
+	 * 
+	 * @see VoltageMonitor#monitorSupplier(Supplier)
+	 */
+	public MonitoredDriveArcade(TankDrivetrain drivetrain, double moveValue, double rotateValue) {
+		super(drivetrain, VoltageMonitor.monitorSupplier(() -> moveValue),
+				VoltageMonitor.monitorSupplier(() -> rotateValue));
+	}
 
 }
