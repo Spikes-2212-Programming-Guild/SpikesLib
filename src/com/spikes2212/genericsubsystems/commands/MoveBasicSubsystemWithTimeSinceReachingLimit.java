01c912d54813d550f68625fe590d81f15f4eff54
omri cohen <omrichoen@gmail.com>
2018-01-31 20:35:03 +0200

    added added new added added new docs for MoveBasicSubsystemWithTimeSinceReachingLimit

--- src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemWithTimeSinceReachingLimit.java
+++ src/com/spikes2212/genericsubsystems/commands/MoveBasicSubsystemWithTimeSinceReachingLimit.java
@@ -7,59 +7,75 @@ import com.spikes2212.genericsubsystems.BasicSubsystem;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
- * This command moves a {@link BasicSubsystem} according to a {@link Supplier}
- * or a constant speed until it can no longer move and then wait a given number
- * of second to make the {@link BasicSubsystem} stick to its end state.
+ * This command moves a BasicSubsystem according to a Supplier or a constant
+ * speed until it cannot move any more, and then wait a given number of second
+ * to make the {@link BasicSubsystem} stick to its end state.
  * 
- *
  * @author Omri "Riki" Cohen
- * @see BasicSubsystem
+ * @see MoveBasicSubsystem
  */
 public class MoveBasicSubsystemWithTimeSinceReachingLimit extends MoveBasicSubsystem {
+
+	/**
+	 * The time the {@link BasicSubsystem} should keep trying to move anter
+	 * reaching its end point.
+	 */
 	protected double waitTime;
+
+	/**
+	 * The last time the {@link BasicSubsystem} has been on the wanted target.
+	 */
 	protected double lastTimeAtEndState = 0;
 
 	/**
-	 * This constructs a new {@link MoveBasicSubsystemWithTimeSinceReachingLimit} command
-	 * using the {@link BasicSubsystem} this command runs on and the supplier
-	 * supplying the speed the {@link BasicSubsystem} should be moved with and a
-	 * wait time.
+	 * This constructs a new
+	 * {@link MoveBasicSubsystemWithTimeSinceReachingLimit} command using the
+	 * {@link BasicSubsystem} this command runs on and the supplier supplying
+	 * the speed the {@link BasicSubsystem} should move with and a wait time.
 	 *
 	 * @param basicSubsystem
 	 *            the {@link BasicSubsystem} this command should move.
 	 * @param speedSupplier
-	 *            a Double {@link Supplier} supplying the speed basicSubsystem
-	 *            should be moved with. Should only supply values between -1 and
+	 *            a Double {@link Supplier} supplying the speed this subsystem
+	 *            should be moved with. Must only supply values between -1 and
 	 *            1.
 	 * @param waitTime
 	 *            the time the {@link BasicSubsystem} should keep trying to move
-	 *            since reaching its end point.
+	 *            anter reaching its end point.
 	 */
-	public MoveBasicSubsystemWithTimeSinceReachingLimit(BasicSubsystem basicSubsystem, Supplier<Double> speed, double waitTime) {
+	public MoveBasicSubsystemWithTimeSinceReachingLimit(BasicSubsystem basicSubsystem, Supplier<Double> speed,
+			double waitTime) {
 		super(basicSubsystem, speed);
 		this.waitTime = waitTime;
 	}
 
 	/**
-	 * This constructs a new {@link MoveBasicSubsystemWithTimeSinceReachingLimit} command
-	 * using the {@link BasicSubsystem} this command runs on and the supplier
-	 * supplying the speed the {@link BasicSubsystem} should be moved with and a
-	 * wait time.
+	 * This constructs a new
+	 * {@link MoveBasicSubsystemWithTimeSinceReachingLimit} command using the
+	 * {@link BasicSubsystem} this command runs on and a constant speed the
+	 * {@link BasicSubsystem} should moved with and a wait time.
 	 *
 	 * @param basicSubsystem
-	 *            the {@link BasicSubsystem} this command should move.
+	 *            the {@link BasicSubsystem} this command opperates on.
 	 * @param speed
-	 *            the speed {@link BasicSubsystem} should be moved with. Should
-	 *            only be values between -1 and 1.
+	 *            the speed this subsystem should be moved with. Values must
+	 *            only be between -1 and 1.
 	 * @param waitTime
 	 *            the time the {@link BasicSubsystem} should keep trying to move
-	 *            since reaching its end point.
+	 *            anter reaching its end point.
 	 */
 	public MoveBasicSubsystemWithTimeSinceReachingLimit(BasicSubsystem basicSubsystem, double speed, double waitTime) {
 		super(basicSubsystem, speed);
 		this.waitTime = waitTime;
 	}
 
+	/**
+	 * Checks if the subsystem can move with the current speed according to the
+	 * limits given in the BasicSubsystem constructor, then checks if the system
+	 * has been on target for the wanted amount time.
+	 * 
+	 * @see MoveBasicSubsystemWithTimeSinceReachingLimit#waitTime
+	 */
 	@Override
 	protected boolean isFinished() {
 		double currentTime = Timer.getFPGATimestamp();
