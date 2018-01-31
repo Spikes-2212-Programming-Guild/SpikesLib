9171ed0f143d1161e65ea6d19fc1ede08bd35281
omri cohen <omrichoen@gmail.com>
2018-01-31 20:29:50 +0200

    added added new docs for MonitoredMoveBasicSubsystem

--- src/com/spikes2212/genericsubsystems/commands/MonitoredMoveBasicSubsystem.java
+++ src/com/spikes2212/genericsubsystems/commands/MonitoredMoveBasicSubsystem.java
@@ -7,40 +7,49 @@ import com.spikes2212.utils.VoltageMonitor;
 
 /**
  * This command works similarly to {@link MoveBasicSubsystem}, only it scales
- * down the speed given to the {@link BasicSubsystem} according to the current
- * voltage.
- * <p>
+ * down the speed according to the current voltage.
+ * 
+ * <br>
+ * <br>
  * The supplier is scaled according to
  * {@link VoltageMonitor#monitorSupplier(Supplier)}, meaning according to the
  * default scaling method and values in {@link VoltageMonitor}.
- * </p>
  *
  * @author Omri "Riki" Cohen and Itamar Rivkind
+ * 
  * @see MoveBasicSubsystem
  * @see VoltageMonitor
  */
 public class MonitoredMoveBasicSubsystem extends MoveBasicSubsystem {
 	
 	/**
-	 * {@inheritDoc}
+	 * This constructs a new {@link MoveBasicSubsystem} command using the
+	 * {@link BasicSubsystem} this command runs on and a supplier supplying the
+	 * speed the {@link BasicSubsystem} should move with.
 	 *
 	 * @param basicSubsystem
-	 *            {@inheritDoc}
+	 *            the {@link BasicSubsystem} this command should move.
 	 * @param speedSupplier
-	 *            {@inheritDoc} Values from this supplier are scaled using
+	 *            a Double {@link Supplier} supplying the speed this subsystem
+	 *            should be moved with. Must only supply values between -1 and 1.
+	 *            This speed is dynamically scaled down according to
 	 *            {@link VoltageMonitor#monitorSupplier(Supplier)}.
 	 */
+
 	public MonitoredMoveBasicSubsystem(BasicSubsystem basicSubsystem, Supplier<Double> speedSupplier) {
 		super(basicSubsystem, VoltageMonitor.monitorSupplier(speedSupplier));
 	}
 
 	/**
-	 * {@inheritDoc}
+	 * This constructs a new {@link MoveBasicSubsystem} command using the
+	 * {@link BasicSubsystem} this command runs on and a constant speed the
+	 * {@link BasicSubsystem} should move with.
 	 *
 	 * @param basicSubsystem
-	 *            {@inheritDoc}
+	 *            the {@link BasicSubsystem} this command opperates on.
 	 * @param speed
-	 *            {@inheritDoc} This speed is dynamically scaled down according
+	 *            the speed this subsystem should be moved with. Values must be
+	 *            between -1 and 1. This speed is dynamically scaled down according
 	 *            to {@link VoltageMonitor#monitorSupplier(Supplier)}.
 	 */
 	public MonitoredMoveBasicSubsystem(BasicSubsystem basicSubsystem, double speed) {
