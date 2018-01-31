ccc38c31e0b17c58f597fba9f3f5555384a1a5eb
omri cohen <omrichoen@gmail.com>
2018-01-31 19:33:40 +0200

    added new docs for CamerasHandler

--- src/com/spikes2212/utils/CamerasHandler.java
+++ src/com/spikes2212/utils/CamerasHandler.java
@@ -18,110 +18,129 @@ import edu.wpi.first.wpilibj.CameraServer;
  * @author Omri "Riki" Cohen & Ittai "Dafner" Dafner
  */
 public class CamerasHandler {
-    private CvSink sink;
-    private Map<Integer, UsbCamera> cameras = new HashMap<>();
+	private CvSink sink;
+	private Map<Integer, UsbCamera> cameras = new HashMap<>();
 
-    /**
-     * Constructs a new instance of {@link CamerasHandler} that automatically
-     * starts streaming to the {@link CameraServer}
-     *
-     * @param width  The width the stream from the cameras should have, in pixels.
-     * @param height The height the stream from the cameras should have, in pixels.
-     * @param ports  The ports of the cameras this handler should start with.
-     */
-    public CamerasHandler(int width, int height, int... ports) {
-        if (ports.length < 1) {
-            throw new IllegalArgumentException("Cannot construct CamerasHandler without cameras");
-        }
-        for (int i : ports) {
-            addCamera(i, width, height);
-        }
-        switchCamera(ports[0]);
-        new Thread(() -> {
-            CvSource outputStream = CameraServer.getInstance().putVideo("CamerasHandler", width, height);
-            Mat frame = new Mat();
-            while (!Thread.interrupted()) {
-                if (sink.grabFrame(frame) == 0) {
-                    // Send the output the error.
-                    outputStream.notifyError(sink.getError());
-                    // skip the rest of the current iteration
-                    continue;
-                }
-                outputStream.putFrame(frame);
-            }
-        }).start();
-    }
+	/**
+	 * Constructs a new instance of {@link CamerasHandler} that automatically starts
+	 * streaming to the <a href=
+	 * "http://first.wpi.edu/FRC/roborio/release/docs/java/edu/wpi/first/wpilibj/CameraServer.html">CameraServer</a>.
+	 *
+	 * @param width
+	 *            the width that the stream from the cameras has, in pixels
+	 * @param height
+	 *            the height that the stream from the cameras has, in pixels
+	 * @param ports
+	 *            the ports of the cameras this handler starts with
+	 * 
+	 * @throws IndexOutOfBoundsException
+	 *             when no camera ports are given
+	 */
+	public CamerasHandler(int width, int height, int... ports) throws IndexOutOfBoundsException {
+		for (int i : ports) {
+			addCamera(i, width, height);
+		}
+		switchCamera(ports[0]);
+		new Thread(() -> {
+			CvSource outputStream = CameraServer.getInstance().putVideo("CamerasHandler", width, height);
+			Mat frame = new Mat();
+			while (!Thread.interrupted()) {
+				if (sink.grabFrame(frame) == 0) {
+					// Send the output the error.
+					outputStream.notifyError(sink.getError());
+					// skip the rest of the current iteration
+					continue;
+				}
+				outputStream.putFrame(frame);
+			}
+		}).start();
+	}
 
-    /**
-     * Switches the stream to the stream from the camera in the given port, if
-     * that camera has been added to this handler. If no camera was added with
-     * that port, does nothing.
-     *
-     * @param port The port of the camera to switch the stream to
-     */
-    public void switchCamera(int port) {
-        if (cameras.containsKey(port)) {
-            this.sink = CameraServer.getInstance().getVideo(cameras.get(port));
-        }
-    }
+	/**
+	 * Switches the stream to the camera with the given port. If there's no camera
+	 * with that port in the {@link CamerasHandler}, does nothing.
+	 *
+	 * @param port
+	 *            the port of the camera to switch to
+	 */
+	public void switchCamera(int port) {
+		if (cameras.containsKey(port)) {
+			this.sink = CameraServer.getInstance().getVideo(cameras.get(port));
+		}
+	}
 
-    /**
-     * Sets the exposure of the camera in the given port, if that camera has
-     * been added to this handler. If no camera was added with that port, does
-     * nothing.
-     *
-     * @param port     The port of the camera to change the exposure for.
-     * @param exposure The exposure the camera should have, in percentage.
-     * @see UsbCamera#setExposureManual(int)
-     */
-    public void setExposure(int exposure, int port) {
-        if (cameras.containsKey(port)) {
-            cameras.get(port).setExposureManual(exposure);
-        }
-    }
+	/**
+	 * Sets the exposure of the camera in the given port. If there's no camera with
+	 * that port in the {@link CamerasHandler}, does nothing.
+	 *
+	 * @param port
+	 *            the port of the said camera
+	 * @param exposure
+	 *            the exposure this camera should have, in percentage
+	 * 
+	 * @see <a href=
+	 *      "http://first.wpi.edu/FRC/roborio/release/docs/java/edu/wpi/cscore/VideoCamera.html#setExposureManual-int-">UsbCamera#setExposureManual(int)</a>
+	 */
+	public void setExposure(int exposure, int port) {
+		if (cameras.containsKey(port)) {
+			cameras.get(port).setExposureManual(exposure);
+		}
+	}
 
-    /**
-     * Sets the exposure of all of the cameras this handler handles.
-     * <p>
-     * Only affect cameras this handler currently handles. <br/>
-     * If a camera is added after running {@link #setExposure(int)}, it will have the default automatic exposure,
-     * meaning you will have to run {@link #setExposure(int, int)} again if you want it to have a none-automatic exposure.
-     * </p>
-     *
-     * @param exposure The exposure the cameras should have, in percentage.
-     * @see UsbCamera#setExposureManual(int)
-     */
-    public void setExposure(int exposure) {
-        for (UsbCamera c : cameras.values()) {
-            c.setExposureManual(exposure);
-        }
-    }
+	/**
+	 * Sets the exposure of all of the cameras this {@link CamerasHandler}.
+	 * 
+	 * <br>
+	 * <br>
+	 * Only affect cameras this handler currently handles. <br>
+	 * If a camera is added after running {@link #setExposure(int)}, it will have
+	 * the default automatic exposure, meaning you will have to run
+	 * {@link #setExposure(int, int)} again if you want it to have none-automatic
+	 * exposure.
+	 * 
+	 * @param exposure
+	 *            the exposure the cameras should have, in percentage
+	 * 
+	 * @see <a href=
+	 *      "http://first.wpi.edu/FRC/roborio/release/docs/java/edu/wpi/cscore/VideoCamera.html#setExposureManual-int-">UsbCamera#setExposureManual(int)</a>
+	 */
+	public void setExposure(int exposure) {
+		for (UsbCamera c : cameras.values()) {
+			c.setExposureManual(exposure);
+		}
+	}
 
-    /**
-     * Adds a camera to this handler using the camera's port. If this handler
-     * already has a camera with that port, does nothing.
-     *
-     * @param port The port of the new camera to add.
-     */
-    public void addCamera(int port) {
-        if (!cameras.containsKey(port)) {
-            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(port);
-            cameras.put(port, camera);
-        }
-    }
+	/**
+	 * Adds a camera to this handler using the camera's port. If this handler
+	 * already has a camera with that port, does nothing.
+	 *
+	 * @param port
+	 *            the port of the new camera
+	 */
+	public void addCamera(int port) {
+		if (!cameras.containsKey(port)) {
+			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(port);
+			cameras.put(port, camera);
+		}
+	}
 
-    /**
-     * Adds a camera to this handler using the camera's port. If this handler
-     * already has a camera with that port, does nothing.
-     *
-     * @param port The port of the new camera to add.
-     */
-    public void addCamera(int port, int width, int height) {
-        if (!cameras.containsKey(port)) {
-            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(port);
-            camera.setResolution(width, height);
-            cameras.put(port, camera);
-        }
-    }
+	/**
+	 * Adds a camera to this handler using the camera's port. If this handler
+	 * already has a camera with that port, does nothing.
+	 *
+	 * @param port
+	 *            the port of the new camera
+	 * @param width
+	 *            the width that the stream from this cameras has, in pixels
+	 * @param height
+	 *            the height that the stream from this cameras has, in pixels
+	 */
+	public void addCamera(int port, int width, int height) {
+		if (!cameras.containsKey(port)) {
+			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(port);
+			camera.setResolution(width, height);
+			cameras.put(port, camera);
+		}
+	}
 
 }
