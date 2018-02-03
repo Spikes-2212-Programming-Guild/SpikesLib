75cf5af1f62ae62f71708d751ed76e9b3aa6ffbf
omri cohen <omrichoen@gmail.com>
2018-02-03 14:35:12 +0200

    ExtendedPIDSettings now extants PIDSettings

--- src/com/spikes2212/utils/ExtendedPIDSettings.java
+++ src/com/spikes2212/utils/ExtendedPIDSettings.java
@@ -13,9 +13,9 @@ import edu.wpi.first.wpilibj.PIDController;
  * @see PIDController
  * 
  */
-public class ExtendedPIDSettings {
+public class ExtendedPIDSettings extends PIDSettings {
 
-	private double KP, KI, KD, tolerance, waitTime;
+	private double tolerance, waitTime;
 
 	/**
 	 * 
@@ -33,9 +33,7 @@ public class ExtendedPIDSettings {
 	 *            setpoint before ending.
 	 */
 	public ExtendedPIDSettings(double KP, double KI, double KD, double tolerance, double waitTime) {
-		this.setKP(KP);
-		this.setKI(KI);
-		this.setKD(KD);
+		super(KP, KI, KD);
 		this.setTolerance(tolerance);
 		this.setWaitTime(waitTime);
 	}
@@ -73,70 +71,6 @@ public class ExtendedPIDSettings {
 	 */
 	public void setTolerance(double tolerance) {
 		this.tolerance = tolerance;
-	}
-
-	/**
-	 * Gets the Proportional coefficient of the PID loop using this settings.
-	 *
-	 * @return the current Proportional coefficient.
-	 * @see PIDController#getP()
-	 */
-	public double getKP() {
-		return KP;
-	}
-
-	/**
-	 * Sets the Proportional coefficient of the PID loop using this settings.
-	 *
-	 * @param KP
-	 *            the new Proportional coefficient.
-	 * @see PIDController#setPID(double, double, double)
-	 */
-	public void setKP(double KP) {
-		this.KP = KP;
-	}
-
-	/**
-	 * Gets the Integral coefficient of the PID loop using this settings.
-	 *
-	 * @return the current Integral coefficient.
-	 * @see PIDController#getI()
-	 */
-	public double getKI() {
-		return KI;
-	}
-
-	/**
-	 * Sets the Integral coefficients of the PID loop using this settings.
-	 *
-	 * @param KI
-	 *            the new Integral coefficient.
-	 * @see PIDController#setPID(double, double, double)
-	 */
-	public void setKI(double KI) {
-		this.KI = KI;
-	}
-
-	/**
-	 * Gets the Differential coefficient of the PID loop using this settings.
-	 *
-	 * @return the current Differential coefficient.
-	 * @see PIDController#getD()
-	 */
-	public double getKD() {
-		return KD;
-	}
-
-	/**
-	 * 
-	 * Sets the Differential coefficient of the PID loop using this settings.
-	 *
-	 * @param KD
-	 *            the new Differential coefficient.
-	 * @see PIDController#setPID(double, double, double)
-	 */
-	public void setKD(double KD) {
-		this.KD = KD;
 	}
 
 	/**
