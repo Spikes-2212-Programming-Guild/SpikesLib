8b020100b9e59c832ed69edfb1226209029bc7f2
Yonatan <yognatan33@gmail.com>
2018-05-31 21:02:10 +0300

    AHhhhh

--- src/com/spikes2212/genericsubsystems/basicSubsystem/commands/MoveBasicSubsystemToTarget.java
+++ src/com/spikes2212/genericsubsystems/basicSubsystem/commands/MoveBasicSubsystemToTarget.java
@@ -6,9 +6,9 @@ import com.spikes2212.genericsubsystems.basicSubsystem.BasicSubsystem;
 import com.spikes2212.genericsubsystems.basicSubsystem.utils.targets.NumericalTarget;
 
 /**
- * This class moves a {@link BasicSubsystem} in a given speed until reaching a
- * target. For example, a light sensor, a limit switch or a specific encoder
- * value (using {@link NumericalTarget}.
+ * This class moves a {@link BasicSubsystem} until reaching a target. For
+ * example, a light sensor, a limit switch or a specific encoder value (using
+ * {@link NumericalTarget}).
  * 
  * @author Omri "Riki" Cohen
  */
@@ -24,8 +24,8 @@ public class MoveBasicSubsystemToTarget extends MoveBasicSubsystem {
 	 * @param basicSubsystem
 	 *            the {@link BasicSubsystem} this command should move.
 	 * @param speedSupplier
-	 *            a Double {@link Supplier} supplying the speed basicSubsystem
-	 *            should be moved with. Should only supply values between -1 and 1.
+	 *            a Double {@link Supplier} supplying the speed subsystem should be
+	 *            moved with. Should only supply values between -1 and 1.
 	 * @param onTarget
 	 *            a Boolean {@link Supplier} returning true when reaching the wanted
 	 *            target.
