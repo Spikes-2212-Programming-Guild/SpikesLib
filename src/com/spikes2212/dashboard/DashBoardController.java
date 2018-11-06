5c7c917063cbace13d0cc90c131901ee74de155e
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-11-06 17:30:29 +0200

    finished fixes

--- src/com/spikes2212/dashboard/DashBoardController.java
+++ src/com/spikes2212/dashboard/DashBoardController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
+import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
