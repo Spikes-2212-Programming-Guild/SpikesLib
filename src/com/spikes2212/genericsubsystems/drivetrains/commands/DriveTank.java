de4a644bcb7e142007b0a514778fad710e4eca74
omri00 <omrichoen@gmail.com>
2018-11-07 09:08:57 +0200

    Update DriveTank.java

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveTank.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveTank.java
@@ -74,7 +74,7 @@ public class DriveTank extends Command {
 
 	// Called once after isFinished returns true
 	protected void end() {
-		tankDrivetrain.tankDrive.stop;
+		tankDrivetrain.tankDrive.stop();
 	}
 
 	// Called when another command which requires one or more of the same
