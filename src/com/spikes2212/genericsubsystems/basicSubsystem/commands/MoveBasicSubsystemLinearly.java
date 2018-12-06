19af4d350085f81ef8e42b3da202d4f2b7a9a9b6
omri <omri@DESKTOP-GDC53VD>
2018-12-06 16:05:51 +0200

    finished MoveBasicSubsystemLinearly

--- src/com/spikes2212/genericsubsystems/basicSubsystem/commands/MoveBasicSubsystemLinearly.java
+++ src/com/spikes2212/genericsubsystems/basicSubsystem/commands/MoveBasicSubsystemLinearly.java
@@ -3,29 +3,37 @@ package com.spikes2212.genericsubsystems.basicSubsystem.commands;
 import java.util.function.Supplier;
 
 import com.spikes2212.genericsubsystems.basicSubsystem.BasicSubsystem;
-/*
+
+/**
  * {@link https://wpilib.screenstepslive.com/s/currentCS/m/java/l/599745-scheduling-commands}
+ * 
  * @author Yuval
  *
  */
-public class MoveBasicSubsystemLinearly extends MoveBasicSubsystem{
-	protected final double currentSpeed=0;
+public class MoveBasicSubsystemLinearly extends MoveBasicSubsystem {
+	private double currentSpeed = 0;
 	protected final double time;
 	protected final double acceleration;
-	public MoveBasicSubsystemLinearly(BasicSubsystem basicSubsystem,
-			double wantedSpeed,double time) {
+
+	public MoveBasicSubsystemLinearly(BasicSubsystem basicSubsystem, Supplier<Double> wantedSpeed, double time) {
 		super(basicSubsystem, wantedSpeed);
-		if(time==0) {
-			time=1;
+		if (time <= 0) {
+			time = 1;
 		}
-		this.basicSubsystem = basicSubsystem;
-		acceleration=(wantedSpeed)/time;                              
+		this.time = time;
+		acceleration = (wantedSpeed.get()) / time;
 	}
-@Override
-public void execute(){
-this.currentSpeed=currentSpeed+acceleration*time/50;
-basicSubsystem.move(currentSpeed);
+
+	public MoveBasicSubsystemLinearly(BasicSubsystem basicSubsystem, double wantedSpeed, double time) {
+		this(basicSubsystem, () -> wantedSpeed, time);
+	}
+
+	@Override
+	public void execute() {
+		currentSpeed = currentSpeed + acceleration * time / 50;
+		if (currentSpeed > speedSupplier.get())
+			currentSpeed = speedSupplier.get();
+		basicSubsystem.move(currentSpeed);
+	}
+
 }
-
-
-}	
