34166122176df643f0d503cc52ffd735d0f1bf61
omri <omri@DESKTOP-GDC53VD>
2018-12-06 16:28:46 +0200

    reset the speed each call

--- src/com/spikes2212/genericsubsystems/basicSubsystem/commands/MoveBasicSubsystemLinearly.java
+++ src/com/spikes2212/genericsubsystems/basicSubsystem/commands/MoveBasicSubsystemLinearly.java
@@ -4,6 +4,8 @@ import java.util.function.Supplier;
 
 import com.spikes2212.genericsubsystems.basicSubsystem.BasicSubsystem;
 
+import edu.wpi.first.wpilibj.Timer;
+
 /**
  * {@link https://wpilib.screenstepslive.com/s/currentCS/m/java/l/599745-scheduling-commands}
  * 
@@ -14,6 +16,7 @@ public class MoveBasicSubsystemLinearly extends MoveBasicSubsystem {
 	private double currentSpeed = 0;
 	protected final double time;
 	protected final double acceleration;
+	private double startTime;
 
 	public MoveBasicSubsystemLinearly(BasicSubsystem basicSubsystem, Supplier<Double> wantedSpeed, double time) {
 		super(basicSubsystem, wantedSpeed);
@@ -21,7 +24,7 @@ public class MoveBasicSubsystemLinearly extends MoveBasicSubsystem {
 			time = 1;
 		}
 		this.time = time;
-		acceleration = (wantedSpeed.get()) / time;
+		acceleration = wantedSpeed.get() / time;
 	}
 
 	public MoveBasicSubsystemLinearly(BasicSubsystem basicSubsystem, double wantedSpeed, double time) {
@@ -29,10 +32,13 @@ public class MoveBasicSubsystemLinearly extends MoveBasicSubsystem {
 	}
 
 	@Override
+	protected void initialize() {
+		startTime = Timer.getFPGATimestamp();
+	}
+
+	@Override
 	public void execute() {
-		currentSpeed = currentSpeed + acceleration * time / 50;
-		if (currentSpeed > speedSupplier.get())
-			currentSpeed = speedSupplier.get();
+		currentSpeed = Math.max(speedSupplier.get(), (Timer.getFPGATimestamp() - startTime) * acceleration);
 		basicSubsystem.move(currentSpeed);
 	}
 
