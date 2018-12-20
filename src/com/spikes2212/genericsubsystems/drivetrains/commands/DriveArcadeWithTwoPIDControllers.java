c1052a2d4271b60bc8cf185be31a7ab9689c13c5
Yuval <Yuval@DESKTOP-9EPK839>
2018-12-20 18:50:26 +0200

    used drivetrain's method.

--- src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveArcadeWithTwoPIDControllers.java
+++ src/com/spikes2212/genericsubsystems/drivetrains/commands/DriveArcadeWithTwoPIDControllers.java
@@ -27,6 +27,7 @@ public class DriveArcadeWithTwoPIDControllers extends Command {
 	protected boolean continuous;
 	private double forwardSpeed;
 	private double rotationSpeed;
+
 	public DriveArcadeWithTwoPIDControllers(TankDrivetrain drivetrain,
 			com.spikes2212.utils.PIDSettings forwardPIDSettings, Supplier<Double> forwardSetpoint,
 			PIDSource forwardSource, com.spikes2212.utils.PIDSettings rotationPIDSettings,
@@ -42,53 +43,56 @@ public class DriveArcadeWithTwoPIDControllers extends Command {
 		this.lastTimeNotOnTarget = lastTimeNotOnTarget;
 		this.inputRange = inputRange;
 		this.continuous = continuous;
-		
+
 	}
 
 	@Override
 	protected void initialize() {
-		forwardMovementControl = new PIDController(forwardPIDSettings.getKP(), forwardPIDSettings.getKI(), forwardPIDSettings.getKD(), forwardSource,
-				(PIDOutput)->forwardSpeed=PIDOutput);
+		forwardMovementControl = new PIDController(forwardPIDSettings.getKP(), forwardPIDSettings.getKI(),
+				forwardPIDSettings.getKD(), forwardSource, (PIDOutput) -> forwardSpeed = PIDOutput);
 		forwardMovementControl.setAbsoluteTolerance(forwardPIDSettings.getTolerance());
 		forwardMovementControl.setSetpoint(this.forwardSetpoint.get());
 		forwardMovementControl.setOutputRange(-1, 1);
 		forwardMovementControl.enable();
-		rotationMovementControl = new PIDController(rotationPIDSettings.getKP(), rotationPIDSettings.getKI(), rotationPIDSettings.getKD(), rotationSource,
-				(PIDOutput1)->rotationSpeed=PIDOutput1);
+		rotationMovementControl = new PIDController(rotationPIDSettings.getKP(), rotationPIDSettings.getKI(),
+				rotationPIDSettings.getKD(), rotationSource, (PIDOutput1) -> rotationSpeed = PIDOutput1);
 		rotationMovementControl.setAbsoluteTolerance(rotationPIDSettings.getTolerance());
 		rotationMovementControl.setSetpoint(this.rotationSetpoint.get());
 		rotationMovementControl.setOutputRange(-1, 1);
 		rotationMovementControl.setInputRange(-inputRange / 2, inputRange / 2);
 		rotationMovementControl.setContinuous(this.continuous);
 		rotationMovementControl.enable();
-	
+
 	}
-	@Override 
+
+	@Override
 	protected void execute() {
-	new DriveArcade(this.drivetrain,forwardSpeed,rotationSpeed);
-	double newSetPointLeft = forwardSetpoint.get();
-	double newSetPointRight = rotationSetpoint.get();
-	if (newSetPointLeft != forwardMovementControl.getSetpoint())
-		forwardMovementControl.setSetpoint(newSetPointLeft);
-	if (newSetPointRight != rotationMovementControl.getSetpoint())
-		rotationMovementControl.setSetpoint(newSetPointRight);
+		
+		drivetrain.arcadeDrive(forwardSpeed,rotationSpeed);
+		double newSetPointLeft = forwardSetpoint.get();
+		double newSetPointRight = rotationSetpoint.get();
+		if (newSetPointLeft != forwardMovementControl.getSetpoint())
+			forwardMovementControl.setSetpoint(newSetPointLeft);
+		if (newSetPointRight != rotationMovementControl.getSetpoint())
+			rotationMovementControl.setSetpoint(newSetPointRight);
 	}
+
 	@Override
 	protected boolean isFinished() {
 		if (!forwardMovementControl.onTarget() || !rotationMovementControl.onTarget()) {
-			lastTimeNotOnTarget = Timer.getFPGATimestamp();	
-	
-	
-	
-	}
+			lastTimeNotOnTarget = Timer.getFPGATimestamp();
+
+		}
 		return Timer.getFPGATimestamp() - lastTimeNotOnTarget >= rotationPIDSettings.getWaitTime();
-}
+	}
+
 	protected void end() {
 		rotationMovementControl.disable();
 		forwardMovementControl.disable();
 		drivetrain.stop();
-	}	
+	}
+
 	protected void interrupted() {
 		end();
 	}
-	}
+}
