d185b2573a004ef7049720dc14d7ebe3834e3211
T <T@DESKTOP-PHDSCAT>
2019-01-15 19:46:14 +0200

    fixed documentation of package-info of utils.targets

--- src/com/spikes2212/utils/targets/package-info.java
+++ src/com/spikes2212/utils/targets/package-info.java
@@ -2,6 +2,9 @@
  * This package contains classes which represents generic targets
  * implementations that can be used as the "target" of a
  * {@link com.spikes2212.genericsubsystems.commands.MoveBasicSubsystemToTarget}
+ * or {@link com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcadeToTarget}
+ * or {@link com.spikes2212.genericsubsystems.drivetrains.commands.DriveTankToTarget}
+ * or {@link com.spikes2212.genericsubsystems.drivetrains.commands.DriveHolonomicToTarget}
  * command. <br>
  *
  * For example - a numerical target with a tolerance. The info of the
