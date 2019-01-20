2276242017e3613f30dd13255d7bbebd9e5bb9a0
T <tuval841@gmail.com>
2019-01-20 18:19:22 +0200

    replaced branch for oneLimits

---
+++ src/com/spikes2212/genericsubsystems/basicSubsystem/utils/limitationFunctions/MaxLimit.java
@@ -0,0 +1,21 @@
+package com.spikes2212.genericsubsystems.basicSubsystem.utils.limitationFunctions;
+
+
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class MaxLimit implements Predicate<Double>{
+	
+	private final Supplier<Boolean> limit;
+	
+	public MaxLimit(Supplier<Boolean> limit) {
+		this.limit = limit;
+	}
+	
+	@Override
+	public boolean test(Double speed) {
+		if(limit.get() && speed > 0) 
+			return false;
+		return true;
+	}
+}
