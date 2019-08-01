c4d3f84163eff71d3bfd67fc75c96b94026f02a0
YuvalLevy1 <Yuval124levy1@gmail.com>
2019-08-01 18:22:44 +0300

    added GearboxSettings.

---
+++ src/motor/GearboxSettings.java
@@ -0,0 +1,36 @@
+package motor;
+
+public class GearboxSettings {
+    int maxCurrent, peakCurrentDuration, continuousCurrentLimit;
+    boolean CurrentLimit = true;
+
+    public GearboxSettings(int maxCurrent, int peakCurrentDuration, int continuousCurrentLimit) {
+        this.maxCurrent = maxCurrent;
+        this.peakCurrentDuration = peakCurrentDuration;
+        this.continuousCurrentLimit = continuousCurrentLimit;
+    }
+
+    public int getMaxCurrent() {
+        return maxCurrent;
+    }
+
+    public void setMaxCurrent(int maxCurrent) {
+        this.maxCurrent = maxCurrent;
+    }
+
+    public int getPeakCurrentDuration() {
+        return peakCurrentDuration;
+    }
+
+    public void setPeakCurrentDuration(int peakCurrentDuration) {
+        this.peakCurrentDuration = peakCurrentDuration;
+    }
+
+    public int getContinuousCurrentLimit() {
+        return continuousCurrentLimit;
+    }
+
+    public void setContinuousCurrentLimit(int continuousCurrentLimit) {
+        this.continuousCurrentLimit = continuousCurrentLimit;
+    }
+}
