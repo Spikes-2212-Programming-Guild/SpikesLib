b6ff87c57f428d60623939acc9d482f753fb01cb
T <tuval841@gmail.com>
2019-08-08 19:08:46 +0300

    added documentation

--- src/com/spikes2212/utils/CANGearbox.java
+++ src/com/spikes2212/utils/CANGearbox.java
@@ -12,6 +12,11 @@ public class CANGearbox implements SpeedController {
 	private WPI_TalonSRX master;
 	private List<BaseMotorController> followers = new ArrayList<>();
 	
+	/**
+	 * Constructs a new Gearbox. All controllers will follow the first TalonSRX.
+	 * @param talon the 'master controller'
+	 * @param controllers all other controllers connected to the gearbox
+	 */
 	public CANGearbox (WPI_TalonSRX talon, BaseMotorController... controllers) {
 		master = talon;
 		for (BaseMotorController controller : controllers) {
@@ -20,36 +25,57 @@ public class CANGearbox implements SpeedController {
 		}
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void set(double speed) {
 		master.set(speed);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public double get() {
 		return master.get();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setInverted(boolean isInverted) {
 		master.setInverted(isInverted);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean getInverted() {
 		return master.getInverted();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void disable() {
 		master.disable();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void stopMotor() {
 		master.stopMotor();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void pidWrite(double output) {
 		master.pidWrite(output);
