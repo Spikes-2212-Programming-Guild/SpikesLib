658c803efc259b1d5fa00276176304548b0f856e
YuvalLevy1 <Yuval124levy1@gmail.com>
2019-09-13 15:51:28 +0300

    changed all addConstant methods in RootNamespace. the methods now use the ConstantHandler methods instead.

--- src/com/spikes2212/utils/RootNamespace.java
+++ src/com/spikes2212/utils/RootNamespace.java
@@ -1,5 +1,6 @@
 package com.spikes2212.utils;
 
+import com.spikes2212.dashboard.ConstantHandler;
 import edu.wpi.first.wpilibj.Preferences;
 
 import java.util.function.Supplier;
@@ -8,22 +9,17 @@ public class RootNamespace implements Namespace {
 
     @Override
     public Supplier<Double> addConstantDouble(String name, double value) {
-        addConstantDouble(name, value);
-        return () -> Preferences.getInstance().getDouble(name, value);
+        return ConstantHandler.addConstantDouble(name, value);
     }
 
     @Override
     public Supplier<Integer> addConstantInt(String name, int value) {
-        addConstantInt(name, value);
-        return () -> Preferences.getInstance().getInt(name, value);
-
+        return ConstantHandler.addConstantInt(name, value);
     }
 
     @Override
     public Supplier<String> addConstantString(String name, String value) {
-        addConstantString(name, value);
-        return () -> Preferences.getInstance().getString(name, value);
-
+       return ConstantHandler.addConstantString(name, value);
     }
 
     @Override
